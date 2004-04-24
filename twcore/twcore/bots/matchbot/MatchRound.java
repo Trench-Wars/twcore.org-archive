d208a67ae969e3469b9ddb583804fd51ed793dcb
fon <fon>
2004-04-24 18:37:39 +0000

    BUG FIX: Started MVP labelling from 1 instead of 0
    git-svn-id: http://www.twcore.org/svn/trunk@71 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/bots/matchbot/MatchRound.java
+++ twcore/twcore/bots/matchbot/MatchRound.java
@@ -551,7 +551,7 @@ public class MatchRound
             if (NUMBER_OF_MVPS > playerList.size())
                 NUMBER_OF_MVPS = playerList.size();
 
-            for (int j = 0; j < NUMBER_OF_MVPS; j++)
+            for (int j = 1; j <= NUMBER_OF_MVPS; j++)
             {
                 MatchPlayer mvp = (MatchPlayer) playerList.get(j);
                 m_logger.sendPrivateMessage(name, "MVP " + j + ": " + mvp.getPlayerName());
