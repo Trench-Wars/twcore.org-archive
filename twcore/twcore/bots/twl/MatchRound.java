1a2c462fc78241e885a0d5beacba70710a37c8d0
lnx <lnx>
2005-04-30 19:40:22 +0000

    if (m_myObjects != null) for m_myObjects.hideAllObjects(); on scoreboard
    git-svn-id: http://www.twcore.org/svn/trunk@521 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/twcore/bots/twl/MatchRound.java
+++ twcore/twcore/bots/twl/MatchRound.java
@@ -1418,7 +1418,8 @@ public class MatchRound
 
     public void do_updateScoreBoard()
     {
-        m_myObjects.hideAllObjects();
+        if (m_myObjects != null)
+            m_myObjects.hideAllObjects();
         m_generalTime -= 1;
         String team1Score;
         String team2Score;
