c23d1e6fe09099a083fb0ab857bcf95611e664e7
sika <sika>
2005-07-20 16:23:16 +0000

    fixed forfeit bug
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@736 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/bots/matchbot/MatchGame.java
+++ twcore/bots/matchbot/MatchGame.java
@@ -488,13 +488,13 @@ public class MatchGame
 
 		int rounds = m_rules.getInt("rounds");
 
-		if (m_curRound.m_team1.isForfeit())
+		if (m_curRound.m_team1.isForfeit() || m_curRound.m_team2.isForfeit())
 		{
-			if (m_curRound.m_team1.teamForfeit())
+			if (m_curRound.m_team1.teamForfeit() && !m_curRound.m_team2.teamForfeit())
 			{
 				m_fnTeam2Score++;
 			}
-			else
+			else if (!m_curRound.m_team1.teamForfeit() && m_curRound.m_team2.teamForfeit())
 			{
 				m_fnTeam1Score++;
 			}
