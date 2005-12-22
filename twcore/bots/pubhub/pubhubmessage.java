886e719439cdb8173453ad2d3e7b9acb0fd16388
jacen <jacen>
2005-12-22 22:03:38 +0000

    messagebot thing
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@929 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/bots/pubhub/pubhubmessage.java
@@ -0,0 +1,30 @@
+package twcore.bots.pubhub;
+
+import twcore.core.*;
+import twcore.misc.pubcommon.*;
+
+public class pubhubmessage extends PubBotModule {
+
+    private String botName;
+
+    /**
+     * This method initializes the pubhubtk module.  It is called after
+     * m_botAction has been initialized.
+     */
+    public void initializeModule() {
+        botName = m_botAction.getBotName();
+    }
+
+
+    /**
+     * Requests the events.  No events.
+     */
+    public void requestEvents(EventRequester eventRequester) {
+    }
+
+    /**
+     * Unimplemented.
+     */
+    public void cancel() {
+    }
+}
