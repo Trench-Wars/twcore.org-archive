bbb4f7a830fa183542175ea1eaefe2d3a191e5fb
Koen Werdler <koen@werdler.com>
2007-03-22 09:24:54 +0000

    These files weren't in the repository but were on bots@multi.strongsite.net (RoboKing).
    git-svn-id: http://www.twcore.org/svn/trunk@1402 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/pubbot/pubbothunt.java
@@ -0,0 +1,269 @@
+// 604 271 8507
+
+package twcore.bots.pubbot;
+
+import java.util.*;
+import java.text.*;
+import java.sql.*;
+
+import twcore.bots.PubBotModule;
+import twcore.core.*;
+import twcore.core.events.PlayerDeath;
+import twcore.core.events.PlayerEntered;
+import twcore.core.events.InterProcessEvent;
+import twcore.core.events.Message;
+import twcore.core.game.Player;
+import twcore.core.util.Tools;
+import twcore.core.util.IPCMessage;
+
+public class pubbothunt extends PubBotModule
+{
+	private String botName;
+	HashMap<String,Integer> playerNames = new HashMap<String,Integer>();
+	TimerTask activity;
+	boolean isRunning = false;
+
+	public void initializeModule()
+	{
+		try {
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT fcVarValue AS val FROM tblSiteVar WHERE fcVarName = 'fbHuntRunning'");
+			results.next();
+			isRunning = new Boolean(results.getString("val"));
+		} catch(Exception e) {e.printStackTrace();}
+		botName = m_botAction.getBotName();
+		if(isRunning) {
+			startHunt();
+			scheduleTasks();
+		}	
+	}
+
+	public void requestEvents(EventRequester eventRequester)
+	{
+		eventRequester.request(EventRequester.MESSAGE);
+		eventRequester.request(EventRequester.PLAYER_DEATH);
+	}
+
+	public void handleEvent(Message event)
+	{
+		int messageType = event.getMessageType();
+		String sender = getSender(event);
+		String message = event.getMessage();
+		if(message.equalsIgnoreCase("!prey")) {
+			getPrey(sender,false);
+		} else if(message.equalsIgnoreCase("!help")) {
+			sendHelp(sender);
+		} else if(message.equalsIgnoreCase("!score")) {
+			getScore(sender);
+		} else if(message.equalsIgnoreCase("!register")) {
+			registerPlayer(sender);
+		}
+	}
+	
+	public void getScore(String name) {
+		if(!playerNames.containsKey(name.toLowerCase())) {
+			m_botAction.sendPrivateMessage(name, "You do not have any points at the moment.");
+			return;
+		}
+		try {
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT fnPoints FROM tblPubHuntCurrent WHERE fnUserID = "+playerNames.get(name.toLowerCase()));
+			if(results.next()) {
+				m_botAction.sendPrivateMessage(name, "You currently have "+results.getInt("fnPoints")+" points.");
+			} else {
+				m_botAction.sendPrivateMessage(name, "You do not have any points at the moment.");
+			}
+		} catch(Exception e) {}
+	}
+	
+	public void registerPlayer(String name) {
+		if(isRunning) {
+			m_botAction.sendPrivateMessage(name, "You can only register when a game is not running.");
+			return;
+		}
+		try {
+			m_botAction.SQLQuery("local", "INSERT INTO tblPubHuntCurrent (fnUserID) (SELECT fnUserID FROM tblUser WHERE fcUserName = '"+Tools.addSlashesToString(name)+"' LIMIT 0,1)");
+			m_botAction.sendPrivateMessage(name, "Registered.");
+		} catch(Exception e) {}
+	}
+	
+	public void sendHelp(String name) {
+		String[] helps = {
+			"!prey          -Tells you your prey",
+			"!score         -Tells you your current score",
+			"!register      -Registers you for next hunt game",
+			"!help          -Sends you this message"
+		};
+		m_botAction.privateMessageSpam(name, helps);
+	}
+
+	public void handleIPC(String botSender, String recipient, String sender, String message)
+	{
+		String command = message.toLowerCase();
+
+		try
+		{
+			if(command.equals("starthunt")) {
+				scheduleTasks();
+				startHunt();
+			} else if(command.equals("endhunt")) {
+				endHunt();
+			}
+		}
+		catch(Exception e)
+		{
+		}
+	}
+
+	public void handleEvent(InterProcessEvent event)
+	{
+		// If the event.getObject() is anything else then the IPCMessage (pubbotchatIPC f.ex) then return
+  	  	if(event.getObject() instanceof IPCMessage == false) { 
+  	  		return;
+  	  	}
+  	  	
+		IPCMessage ipcMessage = (IPCMessage) event.getObject();
+		String message = ipcMessage.getMessage();
+		String recipient = ipcMessage.getRecipient();
+		String sender = ipcMessage.getSender();
+		String botSender = event.getSenderName();
+
+		try
+		{
+			if(recipient == null || recipient.equals(botName))
+			{
+				handleIPC(botSender, recipient, sender, message);
+			}
+		} catch(Exception e) {e.printStackTrace();}
+	}
+
+	public void cancel()
+	{
+	}
+
+	private String getSender(Message event)
+	{
+		int messageType = event.getMessageType();
+
+		if(messageType == Message.REMOTE_PRIVATE_MESSAGE || messageType == Message.CHAT_MESSAGE)
+			return event.getMessager();
+		int senderID = event.getPlayerID();
+		return m_botAction.getPlayerName(senderID);
+	}
+	
+	public void scheduleTasks() {
+		activity = new TimerTask() {
+			public void run() {
+				updateActive();
+			}
+		};
+		m_botAction.scheduleTaskAtFixedRate(activity, getDelay() * 1000, 60 * 1000);
+	}
+	
+	public void updateActive() {
+		Iterator it = playerNames.keySet().iterator();
+		while(it.hasNext()) {
+			String name = (String)it.next();
+			Player p = m_botAction.getPlayer(name);
+			if(p != null && !p.isInSafe() && p.getShipType() != 0)
+				try { m_botAction.SQLQuery("local", getActivityQuery(name));
+				} catch(Exception e) {}
+		}
+	}
+	
+	public String getActivityQuery(String name) {
+		return "UPDATE tblPubHuntCurrent SET fnActivity = fnActivity + 1 WHERE "
+			+ "fnUserID = (SELECT fnUserID FROM tblUser WHERE fcUserName = '"
+			+ Tools.addSlashesToString(name) + "' LIMIT 0,1)";
+	}
+	
+	public void handleEvent(PlayerDeath event) {
+		String hunter = m_botAction.getPlayerName(event.getKillerID()).toLowerCase();
+		String huntee = m_botAction.getPlayerName(event.getKilleeID()).toLowerCase();
+		if(hunter == null || huntee == null) return;
+		if(playerNames.containsKey(hunter) && playerNames.containsKey(huntee)) {
+			try {
+				ResultSet results = m_botAction.SQLQuery("local", "SELECT * FROM tblPubHuntCurrent WHERE fnUserID = "+playerNames.get(hunter)+" AND fnPreyID = "+playerNames.get(huntee));
+				if(results.next()) {
+					delayHuntedTell(huntee);
+					ResultSet result2 = m_botAction.SQLQuery("local", "SELECT * FROM tblPubHuntCurrent WHERE fnUserID = "+playerNames.get(huntee));
+					result2.next();
+					m_botAction.SQLQuery("local", "UPDATE tblPubHuntCurrent SET fnPoints = fnPoints + 1 + "+result2.getInt("fnPoints")+", "
+						+ "fnPreyID = "+result2.getInt("fnPreyID")+" WHERE fnUserID = "+playerNames.get(hunter));
+					m_botAction.SQLQuery("local", "DELETE FROM tblPubHuntCurrent WHERE fnUserID = "+playerNames.get(huntee));
+					ResultSet results3 = m_botAction.SQLQuery("local", "SELECT * FROM tblPubHuntCurrent");
+					int playersLeft = 0;
+					while(results3.next()) playersLeft++;
+					if(playersLeft == 1) {
+						m_botAction.ipcSendMessage("pubBots", "huntover", "PubHub", botName);
+						m_botAction.sendPrivateMessage(hunter, "Congratulations, you have won this round of hunt.");
+					} else {
+						getPrey(hunter, true);
+					}
+				}
+			} catch(Exception e) {e.printStackTrace();}
+		}
+	}
+	
+	public void delayHuntedTell(String name) {
+		final String playerHunted = name;
+		TimerTask tellPlayer = new TimerTask() {
+			public void run() {
+				m_botAction.sendSmartPrivateMessage(playerHunted, "You have been hunted.");
+			}
+		};
+		m_botAction.scheduleTask(tellPlayer, 30 * 1000);
+	}
+	
+	public void handleEvent(PlayerEntered event) {
+		String name = m_botAction.getPlayerName(event.getPlayerID());
+		if(playerNames.containsKey(name.toLowerCase())) {
+			getPrey(name,true);
+		}
+	}
+	
+	public void getPrey(String name, boolean entered) {
+		try {
+			ResultSet results2 = m_botAction.SQLQuery("local", 
+				"SELECT fcUserName AS name FROM tblUser AS u, tblPubHuntCurrent AS phc "
+				+ "WHERE phc.fnUserID = "+playerNames.get(name.toLowerCase())+" AND phc.fnPreyID = u.fnUserID");
+			if(results2.next())
+				m_botAction.sendPrivateMessage(name, "Your current prey is: "+results2.getString("name"));
+			else if(!entered) {
+				m_botAction.sendPrivateMessage(name, "You do not currently have a prey.");
+			}
+		} catch(Exception e) {}
+	}
+	
+	public void startHunt() {
+		isRunning = true;
+		try {
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT fcUserName AS name, u.fnUserID AS id FROM tblUser AS u, tblPubHuntCurrent AS phc WHERE phc.fnUserID = u.fnUserID");
+			while(results.next()) {
+				playerNames.put(results.getString("name").toLowerCase(),results.getInt("id"));
+				if(m_botAction.getPlayer(results.getString("name")) != null) {
+					ResultSet results2 = m_botAction.SQLQuery("local", 
+						"SELECT fcUserName AS name FROM tblUser AS u, tblPubHuntCurrent AS phc "
+						+ "WHERE phc.fnUserID = "+results.getInt("id")+" AND phc.fnPreyID = u.fnUserID");
+					results2.next();
+					m_botAction.sendPrivateMessage(results.getString("name"), "Your current prey is: "+results2.getString("name"));
+				}
+			}
+		} catch(Exception e) {}
+	}
+	
+	public void endHunt() {
+		try { activity.cancel(); } catch(Exception e) {}
+		playerNames.clear();
+		isRunning = false;
+	}
+	
+	private int getDelay() {
+		java.util.Date d = new java.util.Date();
+
+		SimpleDateFormat formatter = new SimpleDateFormat("ss");
+		int seconds = Integer.parseInt( formatter.format( d ) );
+
+		int secondsTill = 60 - seconds;
+
+		return secondsTill;
+	}
+}
---
+++ twcore/src/twcore/bots/pubhub/pubhubhunt.java
@@ -0,0 +1,179 @@
+package twcore.bots.pubhub;
+
+import java.util.*;
+import java.sql.*;
+import java.text.*;
+
+import twcore.bots.PubBotModule;
+import twcore.core.*;
+import twcore.core.events.InterProcessEvent;
+import twcore.core.events.Message;
+import twcore.core.util.IPCMessage;
+
+public class pubhubhunt extends PubBotModule
+{
+	
+	boolean isRunning = false;
+	String botName;
+	TimerTask checkActivity;
+	public void initializeModule()
+	{
+		try {
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT fcVarValue AS val FROM tblSiteVar WHERE fcVarName = 'fbHuntRunning'");
+			results.next();
+			isRunning = new Boolean(results.getString("val").equals("true"));
+		} catch(Exception e) {e.printStackTrace();}
+		botName = m_botAction.getBotName();
+	}
+
+	public void requestEvents(EventRequester eventRequester)
+	{
+		eventRequester.request(EventRequester.MESSAGE);
+	}
+
+	public void handleChatMessage(String sender, String message)
+	{
+		if(message.equalsIgnoreCase("!starthunt")) {
+			startHunt();
+		}
+	}
+
+	public void handleEvent(Message event)
+	{
+		String sender = event.getMessager();
+		String message = event.getMessage();
+		int messageType = event.getMessageType();
+		if(messageType == Message.CHAT_MESSAGE)
+		{
+			handleChatMessage(sender, message);
+		}
+	}
+
+	public void handleIPC(String botSender, String recipient, String sender, String message)
+	{
+		String command = message.toLowerCase();
+
+		try
+		{
+			if(command.equals("huntover")) {
+				endHunt();
+			}
+		}
+		catch(Exception e)
+		{
+		}
+	}
+
+	public void handleEvent(InterProcessEvent event)
+	{
+		// If the event.getObject() is anything else then the IPCMessage (pubbotchatIPC f.ex) then return
+  	  	if(event.getObject() instanceof IPCMessage == false) { 
+  	  		return;
+  	  	}
+  	  	
+		IPCMessage ipcMessage = (IPCMessage) event.getObject();
+		String message = ipcMessage.getMessage();
+		String recipient = ipcMessage.getRecipient();
+		String sender = ipcMessage.getSender();
+		String botSender = event.getSenderName();
+
+		if(recipient == null || recipient.equals(botName))
+			handleIPC(botSender, recipient, sender, message);
+	}
+
+	public void cancel()
+	{
+	}
+	
+	public void endHunt() {
+		if(!isRunning) return;
+		m_botAction.ipcSendMessage("pubBots", "endhunt", null, "PubHub");
+		try {
+			m_botAction.SQLQuery("local", "UPDATE tblSiteVar SET fcVarValue = 'false' WHERE fcVarName = 'fbHuntRunning'");
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT * FROM tblPubHuntPlayer WHERE fnUserID = (SELECT fnUserID FROM tblPubHuntCurrent)");
+			if(results == null || !results.next())
+				m_botAction.SQLQuery("local", "INSERT INTO tblPubHuntPlayer (fnUserID, fnPoints) "
+					+ "(SELECT fnUserID, fnPoints FROM tblPubHuntCurrent)");
+			else 
+				m_botAction.SQLQuery("local", "UPDATE tblPubHuntPlayer SET fnPoints = fnPoints + (SELECT fnPoints FROM tblPubHuntCurrent) "
+					+ "WHERE fnUserID = (SELECT fnUserID FROM tblPubHuntCurrent)");
+			m_botAction.SQLQuery("local", "TRUNCATE TABLE tblPubHuntCurrent");
+		} catch(Exception e) {e.printStackTrace();}
+		isRunning = false;
+	}	
+	
+	public void startHunt() {
+		if(isRunning) return;
+		try {
+			ArrayList<Integer> userids = new ArrayList<Integer>();
+			int k = 0;
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT fnUserID FROM tblPubHuntCurrent");
+			while(results.next()) {
+				userids.add((Integer)results.getInt("fnUserID"));
+			}
+			Random rand = new Random();
+			ArrayList<Integer> huntOrder = new ArrayList<Integer>();
+			while(userids.size() > 0) {
+				huntOrder.add(userids.remove(rand.nextInt(userids.size())));
+			}
+			for(k = 0;k < huntOrder.size();k++) {
+				int prey;
+				if(k > 0) prey = huntOrder.get(k-1);
+				else prey = huntOrder.get(huntOrder.size()-1);
+				int hunter = huntOrder.get(k);
+				m_botAction.SQLQuery("local", "UPDATE tblPubHuntCurrent SET fnPreyID = "+prey+" WHERE fnUserID = "+hunter);
+			}
+			m_botAction.SQLQuery("local", "UPDATE tblSiteVar SET fcVarValue = 'true' WHERE fcVarName = 'fbHuntRunning'");
+		} catch(Exception e) {e.printStackTrace();}
+		m_botAction.ipcSendMessage("pubBots", "starthunt", null, "PubHub");
+		scheduleTasks();
+		isRunning = true;
+	}
+	
+	public void scheduleTasks() {
+		checkActivity = new TimerTask() {
+			public void run() {
+				checkActivity();
+			}
+		};
+		m_botAction.scheduleTaskAtFixedRate(checkActivity, getDelay()*1000, 60 * 1000);
+	}
+	
+	public void checkActivity() {
+		if(!isRunning) return;
+		
+		try {
+			ResultSet results = m_botAction.SQLQuery("local", "SELECT MAX(fnActivity) AS max FROM tblPubHuntCurrent");
+			results.next();
+			int minAct = results.getInt("max") - 15;
+			results = m_botAction.SQLQuery("local", "SELECT fnUserID, fnPreyID FROM tblPubHuntCurrent WHERE fnActivity < "+minAct);
+			while(results.next()) {
+				ResultSet result2 = m_botAction.SQLQuery("local", 
+					  "SELECT phc.fnUserID AS uid, u.fcUserName AS hunter, u2.fcUserName AS newprey "
+					+ "FROM tblUser AS u, tblPubHuntCurrent AS phc, tblUser AS u2 "
+					+ "WHERE u.fnUserID = phc.fnUserID AND u2.fnUserID = "+results.getInt("fnPreyID") + " "
+					+ "AND phc.fnPreyID = "+results.getInt("fnUserID"));
+				m_botAction.SQLQuery("local", "DELETE FROM tblPubHuntCurrent WHERE fnUserID = "+results.getInt("fnUserID"));
+				if(result2.next() && !result2.getString("hunter").equals(result2.getString("newprey"))) {
+					m_botAction.SQLQuery("local", "UPDATE tblPubHuntCurrent SET fnPreyID = "+results.getInt("fnPreyID")+" "
+					+ "WHERE fnUserID = "+result2.getInt("uid"));
+					m_botAction.sendSmartPrivateMessage(result2.getString("hunter"), "Your new prey is "+result2.getString("newprey"));
+				} else {
+					m_botAction.sendSmartPrivateMessage(result2.getString("hunter"), "Congratulations, you have won this round of hunt.");
+					endHunt();	
+				}
+			}
+		} catch(Exception e) {e.printStackTrace();}
+	}
+	
+	private int getDelay() {
+		java.util.Date d = new java.util.Date();
+
+		SimpleDateFormat formatter = new SimpleDateFormat("ss");
+		int seconds = Integer.parseInt( formatter.format( d ) );
+
+		int secondsTill = 70 - seconds;
+
+		return secondsTill;
+	}
+}
