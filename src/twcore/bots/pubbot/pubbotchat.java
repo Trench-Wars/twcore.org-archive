457472969fc3d62bc41ca895ab8635ae83153531
Koen Werdler <koen@werdler.com>
2007-03-22 15:15:16 +0000

    New Pubbot/Pubhub module: chat (pubbotchat & pubhubchat) This module records the chat in the arena the pubbot is in so it can be viewed on trenchwars.org.
    The module also features a !last # command for staff to catch 
    racist players or to read things that were said. The latter 
    isn't stored in the database so it's cleared when the bot 
    goes down.
    
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@1404 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ src/twcore/bots/pubbot/pubbotchat.java
@@ -0,0 +1,165 @@
+package twcore.bots.pubbot;
+
+import java.util.Stack;
+
+import twcore.bots.PubBotModule;
+import twcore.core.EventRequester;
+import twcore.core.events.ArenaList;
+import twcore.core.events.Message;
+import twcore.core.util.Tools;
+
+public class pubbotchat extends PubBotModule {
+	private String currentArena;
+	private String botName;
+	
+	private Stack<String> chat = new Stack<String>();
+	private int maxSize = 20;
+
+	/**
+	 * Initialize this module
+	 */
+	public void initializeModule() {
+		currentArena = m_botAction.getArenaName();
+		botName = m_botAction.getBotName();
+		
+	}
+
+	/**
+	 * Requests the necessary events for this module to work properly
+	 */
+	public void requestEvents(EventRequester eventRequester) {
+		eventRequester.request(EventRequester.MESSAGE);
+		eventRequester.request(EventRequester.ARENA_LIST);
+	}
+
+	/**
+	 * Sends each chat message that should be recorded to the pubhub
+	 */
+	public void handleEvent(Message event) {
+		// Ignore commands send to the bot
+		if( event.getMessageType() == Message.PRIVATE_MESSAGE &&
+			event.getMessage().startsWith("!")) {
+			
+			String name = m_botAction.getPlayerName( event.getPlayerID() );
+			
+			// !help
+			if(m_botAction.getOperatorList().isZH(name) && event.getMessage().toLowerCase().startsWith("!help")) {
+				m_botAction.sendPrivateMessage( name, "Pubbot Chat Module" );
+            	m_botAction.sendPrivateMessage( name, "!help        - this message");
+            	m_botAction.sendPrivateMessage( name, "!last #      - returns # previous recorded chat lines (max. "+maxSize+")");
+			}
+			// !last
+			if(m_botAction.getOperatorList().isZH(name) && event.getMessage().toLowerCase().startsWith("!last ")) {
+				String arg = event.getMessage().substring(6).toLowerCase();
+				
+				if(Tools.isAllDigits(arg)) {
+					int lines = Integer.parseInt(arg);
+					if(lines > maxSize)
+						lines = maxSize;
+					if(lines > chat.size()) 
+						lines = chat.size();
+					
+					for(int i = lines-1 ; i >= 0 ; i--) {
+						if(chat.get(i)!=null)
+							m_botAction.sendPrivateMessage(name, chat.get(i));
+					}
+				} else {
+					m_botAction.sendPrivateMessage(name, "Syntax error. Please use the following syntax: !last <NUMBER of previous chat lines to display>");
+				}
+			}
+			return;
+		}
+		
+		// Ignore arena messages that the bot shouldn't record
+		if( event.getMessageType() == Message.ARENA_MESSAGE &&
+			(
+				event.getMessage().equals("Message has been sent to online moderators") ||
+				event.getMessage().equals("Reliable kill messages ON") ||
+				event.getMessage().startsWith("IP:") ||			// Below are arena message from *info
+				event.getMessage().startsWith("MachineId:") ||
+				event.getMessage().startsWith("Ping:") ||
+				event.getMessage().startsWith("LOSS: S2C:") ||
+				event.getMessage().startsWith("S2C:") ||
+				event.getMessage().startsWith("C2S CURRENT: Slow:") ||
+				event.getMessage().startsWith("S2C CURRENT: Slow:") ||
+				event.getMessage().startsWith("TIME: Session: ") ||
+				event.getMessage().startsWith("Bytes/Sec:")
+			)) {
+			return;
+		}
+		
+		if(	event.getMessageType() == Message.ARENA_MESSAGE ||
+			event.getMessageType() == Message.OPPOSING_TEAM_MESSAGE ||
+			event.getMessageType() == Message.PRIVATE_MESSAGE ||
+			event.getMessageType() == Message.PUBLIC_MACRO_MESSAGE ||
+			event.getMessageType() == Message.PUBLIC_MESSAGE ||
+			event.getMessageType() == Message.REMOTE_PRIVATE_MESSAGE ||
+			event.getMessageType() == Message.TEAM_MESSAGE) {
+			
+			String sender = getSender(event);
+			// If the sender is null then it's probably an Arena message
+			
+			pubbotchatIPC ipc = new pubbotchatIPC(currentArena, event.getMessageType(), sender, event.getMessage(), botName, this.getPubHubName());
+			m_botAction.ipcTransmit(pubbot.IPCCHAT, ipc);
+			
+			chat.insertElementAt(getMessageTypeString(event.getMessageType()) + "  " + (sender==null ? "" : sender+"> ") + event.getMessage(),0);
+			chat.setSize(maxSize);
+		}
+	}
+
+	public void handleEvent(ArenaList event) {
+		currentArena = m_botAction.getArenaName();
+	}
+
+	@Override
+	public void cancel() {}
+	
+	/**
+	 * This method gets the sender from a message Event.
+	 *
+	 * @param event is the message event to analyze.
+	 * @return the name of the sender is returned.  If the sender cannot be
+	 * determined then null is returned.
+	 */
+  	private String getSender(Message event) {
+  		int messageType = event.getMessageType();
+
+  		if(messageType == Message.REMOTE_PRIVATE_MESSAGE || messageType == Message.CHAT_MESSAGE)
+  			return event.getMessager();
+  		int senderID = event.getPlayerID();
+  		return m_botAction.getPlayerName(senderID);
+  	}
+  	
+  	/**
+  	 * Returns a two-letter combination which can be used to identify the message type
+  	 * @param messageType
+  	 * @return
+  	 */
+  	private String getMessageTypeString(int messageType)
+    {
+      switch(messageType)
+      {
+        case Message.PUBLIC_MESSAGE:
+          return "  ";
+        case Message.PRIVATE_MESSAGE:
+          return "P ";
+        case Message.TEAM_MESSAGE:
+          return "T ";
+        case Message.OPPOSING_TEAM_MESSAGE:
+          return "OT";
+        case Message.ARENA_MESSAGE:
+          return "";
+        case Message.PUBLIC_MACRO_MESSAGE:
+          return "MC";
+        case Message.REMOTE_PRIVATE_MESSAGE:
+          return "RP";
+        /*case Message.WARNING_MESSAGE:
+          return "W";
+        case Message.SERVER_ERROR:
+          return "SE";
+        case Message.ALERT_MESSAGE:
+          return "AL";*/
+      }
+      return "??";
+    }
+}
