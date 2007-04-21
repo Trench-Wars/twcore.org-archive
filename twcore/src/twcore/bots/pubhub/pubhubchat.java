47144c96485904808a7be605db8ecbb77c5690a0
Koen Werdler <koen@werdler.com>
2007-04-21 21:39:31 +0000

    added temporary debug messages
    git-svn-id: http://www.twcore.org/svn/trunk@1445 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubhub/pubhubchat.java
+++ twcore/src/twcore/bots/pubhub/pubhubchat.java
@@ -35,6 +35,8 @@ public class pubhubchat extends PubBotModule {
 	@Override
 	public void handleEvent(InterProcessEvent event) {
 		if(event.getObject() instanceof pubbotchatIPC) {
+			m_botAction.sendTeamMessage("pubbotchatIPC");
+			
 			pubbotchatIPC ipc = (pubbotchatIPC)event.getObject();
 			String arena = Tools.addSlashesToString(ipc.getArena());
 			String message = Tools.addSlashesToString(ipc.getMessage());
@@ -43,19 +45,23 @@ public class pubhubchat extends PubBotModule {
 			
 			// Check if the HashSet for this arena already exists, if not, create it
 			if(arenaIDs.containsKey(arena)) {
+				m_botAction.sendTeamMessage("arena exists");
 				ids = arenaIDs.get(arena);
 			} else {	// Create the HashSet and put it to the HashMap
 				ids = new Vector<Integer>(maxSavedLines);
 				arenaIDs.put(arena, ids);
+				m_botAction.sendTeamMessage("new arena");
 			}
 			
 			// Save the message
+			m_botAction.sendTeamMessage("save chat to db");
 			try {
 			    m_botAction.SQLQueryAndClose( dbConn, "INSERT INTO tblChat (`fcArena`, `fnMessageType`, `fcPlayername`, `fcMessage`) VALUES ('"+arena+"', "+ipc.getMessageType()+", '"+sender+"', '"+message+"')" );
 			} catch(SQLException sqle) {
 				Tools.printStackTrace(sqle);
 			}
 			
+			m_botAction.sendTeamMessage("getting latest inserted id");
 			// Add the inserted ID to the map tracking the IDs
 			try {
 			    ResultSet rs = m_botAction.SQLQuery( dbConn, "SELECT LAST_INSERT_ID()" );
@@ -63,12 +69,15 @@ public class pubhubchat extends PubBotModule {
 			    	ids.add(0,rs.getInt(1));
 			    	ids.setSize(maxSavedLines);
 			    }
+			    m_botAction.SQLClose(rs);
 			} catch(SQLException sqle) {
 				Tools.printStackTrace(sqle);
 			}
 			
 			// Clean up
+			m_botAction.sendTeamMessage("cleanup necessary?");
 			if(ids.get(19) != null) {
+				m_botAction.sendTeamMessage("yes");
 				try {
 					m_botAction.SQLQueryAndClose( dbConn, "DELETE FROM tblChat WHERE fcArena = '"+arena+"' AND fnChatID < "+ids.get(19));
 				} catch (SQLException sqle) {
