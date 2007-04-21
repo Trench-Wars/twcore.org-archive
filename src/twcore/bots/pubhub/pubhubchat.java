07aff1db35b3969416ee1b67d4842c70f0f73815
Koen Werdler <koen@werdler.com>
2007-04-21 20:35:09 +0000

    I made a few updates to the chat module of pubhub so it's less database intensive. (A HashMap/HashSet combination now tracks the IDs of previous chat lines to tell which chat lines should be deleted.)
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@1443 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubhub/pubhubchat.java
+++ src/twcore/bots/pubhub/pubhubchat.java
@@ -2,17 +2,20 @@ package twcore.bots.pubhub;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Vector;
 
 import twcore.bots.PubBotModule;
 import twcore.bots.pubbot.pubbotchatIPC;
 import twcore.core.EventRequester;
 import twcore.core.events.InterProcessEvent;
 import twcore.core.events.Message;
-import twcore.core.stats.DBPlayerData;
 import twcore.core.util.Tools;
 
 public class pubhubchat extends PubBotModule {
 
+	private HashMap<String, Vector> arenaIDs = new HashMap<String, Vector>(10);
+	
 	private final String dbConn = "local";
 	private final int maxSavedLines = 20;
 	
@@ -36,6 +39,15 @@ public class pubhubchat extends PubBotModule {
 			String arena = Tools.addSlashesToString(ipc.getArena());
 			String message = Tools.addSlashesToString(ipc.getMessage());
 			String sender = (ipc.getMessageType() == Message.ARENA_MESSAGE) ? null : ipc.getSender();
+			Vector<Integer> ids;
+			
+			// Check if the HashSet for this arena already exists, if not, create it
+			if(arenaIDs.containsKey(arena)) {
+				ids = arenaIDs.get(arena);
+			} else {	// Create the HashSet and put it to the HashMap
+				ids = new Vector<Integer>(maxSavedLines);
+				arenaIDs.put(arena, ids);
+			}
 			
 			// Save the message
 			try {
@@ -44,20 +56,24 @@ public class pubhubchat extends PubBotModule {
 				Tools.printStackTrace(sqle);
 			}
 			
-			// Clean up
+			// Add the inserted ID to the map tracking the IDs
 			try {
-				ResultSet resultSet = m_botAction.SQLQuery( dbConn, "SELECT fnChatId FROM tblChat WHERE fcArena = '"+arena+"'");
-				if(resultSet != null && resultSet.next()) {
-					resultSet.afterLast();					// Go to the last row
-					resultSet.relative(-maxSavedLines);		// Move up a number of rows to save
-					if(resultSet.isBeforeFirst() == false && resultSet.isAfterLast() == false) {
-						int fnChatID = resultSet.getInt("fnChatID"); // Get the fnChatID of this row
-						m_botAction.SQLQueryAndClose( dbConn, "DELETE FROM tblChat WHERE fcArena = '"+arena+"' AND fnChatID < "+fnChatID);
-					}
-				}
-                                m_botAction.SQLClose( resultSet );
-			} catch (SQLException sqle) {
+			    ResultSet rs = m_botAction.SQLQuery( dbConn, "SELECT LAST_INSERT_ID()" );
+			    if(rs != null && rs.next()) {
+			    	ids.add(0,rs.getInt(1));
+			    	ids.setSize(maxSavedLines);
+			    }
+			} catch(SQLException sqle) {
 				Tools.printStackTrace(sqle);
+			}
+			
+			// Clean up
+			if(ids.get(19) != null) {
+				try {
+					m_botAction.SQLQueryAndClose( dbConn, "DELETE FROM tblChat WHERE fcArena = '"+arena+"' AND fnChatID < "+ids.get(19));
+				} catch (SQLException sqle) {
+					Tools.printStackTrace(sqle);
+				}
 			}
 		}
 	}
