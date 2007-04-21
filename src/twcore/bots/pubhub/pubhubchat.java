b53ebe81f7e1f2464676949a75d31f3fa8059441
Koen Werdler <koen@werdler.com>
2007-04-21 22:21:48 +0000

    Removed the debug messages
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@1446 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubhub/pubhubchat.java
+++ src/twcore/bots/pubhub/pubhubchat.java
@@ -14,7 +14,7 @@ import twcore.core.util.Tools;
 
 public class pubhubchat extends PubBotModule {
 
-	private HashMap<String, Vector> arenaIDs = new HashMap<String, Vector>(10);
+	private HashMap<String, Vector<Integer>> arenaIDs = new HashMap<String, Vector<Integer>>(10);
 	
 	private final String dbConn = "local";
 	private final int maxSavedLines = 20;
@@ -34,9 +34,7 @@ public class pubhubchat extends PubBotModule {
 
 	@Override
 	public void handleEvent(InterProcessEvent event) {
-		if(event.getObject() instanceof pubbotchatIPC) {
-			m_botAction.sendTeamMessage("pubbotchatIPC");
-			
+		if(event.getChannel().equals(pubhub.IPCCHAT) && event.getObject() instanceof pubbotchatIPC) {
 			pubbotchatIPC ipc = (pubbotchatIPC)event.getObject();
 			String arena = Tools.addSlashesToString(ipc.getArena());
 			String message = Tools.addSlashesToString(ipc.getMessage());
@@ -45,23 +43,19 @@ public class pubhubchat extends PubBotModule {
 			
 			// Check if the HashSet for this arena already exists, if not, create it
 			if(arenaIDs.containsKey(arena)) {
-				m_botAction.sendTeamMessage("arena exists");
 				ids = arenaIDs.get(arena);
 			} else {	// Create the HashSet and put it to the HashMap
 				ids = new Vector<Integer>(maxSavedLines);
 				arenaIDs.put(arena, ids);
-				m_botAction.sendTeamMessage("new arena");
 			}
 			
 			// Save the message
-			m_botAction.sendTeamMessage("save chat to db");
 			try {
 			    m_botAction.SQLQueryAndClose( dbConn, "INSERT INTO tblChat (`fcArena`, `fnMessageType`, `fcPlayername`, `fcMessage`) VALUES ('"+arena+"', "+ipc.getMessageType()+", '"+sender+"', '"+message+"')" );
 			} catch(SQLException sqle) {
 				Tools.printStackTrace(sqle);
 			}
 			
-			m_botAction.sendTeamMessage("getting latest inserted id");
 			// Add the inserted ID to the map tracking the IDs
 			try {
 			    ResultSet rs = m_botAction.SQLQuery( dbConn, "SELECT LAST_INSERT_ID()" );
@@ -75,9 +69,7 @@ public class pubhubchat extends PubBotModule {
 			}
 			
 			// Clean up
-			m_botAction.sendTeamMessage("cleanup necessary?");
 			if(ids.get(19) != null) {
-				m_botAction.sendTeamMessage("yes");
 				try {
 					m_botAction.SQLQueryAndClose( dbConn, "DELETE FROM tblChat WHERE fcArena = '"+arena+"' AND fnChatID < "+ids.get(19));
 				} catch (SQLException sqle) {
