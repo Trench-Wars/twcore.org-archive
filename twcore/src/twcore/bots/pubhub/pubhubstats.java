f2c7e4fa0604727c34aa203406e273a20d104744
flibb <flibb>
2007-12-16 02:12:54 +0000

    suppress unchecked warnings where unavoidable or intended
    git-svn-id: http://www.twcore.org/svn/trunk@2016 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubhub/pubhubstats.java
+++ twcore/src/twcore/bots/pubhub/pubhubstats.java
@@ -12,18 +12,18 @@ import twcore.core.util.Tools;
 public class pubhubstats extends PubBotModule {
 	//private String database = "pubstats";
 	private String database = "server";
-	
+
 	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
+
 	// boolean to immediately stop execution
 	private boolean stop = false;
-	
+
 	/**
 	 * This method initializes the pubhubstats module.  It is called after
 	 * m_botAction has been initialized.
 	 */
 	public void initializeModule() {}
-	
+
 	/**
 	 * Unused method but needs to be overridden
 	 * @see Module.requestEvents()
@@ -31,15 +31,16 @@ public class pubhubstats extends PubBotModule {
 	public void requestEvents(EventRequester eventRequester) {}
 
 	/**
-	 * This method handles the incoming InterProcessEvents, 
-	 * checks if the IPC is meant for this module and then extracts the containing 
+	 * This method handles the incoming InterProcessEvents,
+	 * checks if the IPC is meant for this module and then extracts the containing
 	 * object to store it to database.
 	 *
 	 * @param event is the InterProcessEvent to handle.
 	 */
+	@SuppressWarnings("unchecked")
 	public void handleEvent(InterProcessEvent event)
 	{
-		
+
 		// If the event.getObject() is anything else then the HashMap then return
 		if(event.getObject() instanceof HashMap == false) {
 			return;
@@ -47,7 +48,7 @@ public class pubhubstats extends PubBotModule {
 		if(event.getChannel().equals(pubhub.IPCPUBSTATS) == false) {
 			return;
 		}
-	  
+
 		HashMap<String, PubStatsScore> stats = (HashMap<String, PubStatsScore>)event.getObject();
 		updateDatabase(stats.values());
   }
@@ -58,18 +59,18 @@ public class pubhubstats extends PubBotModule {
 	public void cancel() {
 		stop = true;
 	}
-  
+
 	/**
 	 * Stores all the data from the PubStats objects into the tblScore table in the database
-	 * 
+	 *
 	 * @param stats the Collection containing the PubStats objects
 	 */
 	private void updateDatabase(Collection<PubStatsScore> stats) {
 		// Loop over all the PubStats objects and replace each in the stats table
 		/*for(PubStatsScore pubstats:stats) {
 			if(stop) break;
-		  
-			String query = 
+
+			String query =
 				"REPLACE INTO tblScore VALUES (" +
 				"'" + Tools.addSlashesToString(pubstats.getPlayername()) + "'," +
 				pubstats.getShip() + "," +
