21dc96ff61d16d9adca5b7d83c33e1291991a0de
Koen Werdler <koen@werdler.com>
2008-03-28 20:35:43 +0000

    The !watches are now saved to the .cfg file on each change.
    See ticket #217
    
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@2436 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubhub/pubhubalias.java
+++ src/twcore/bots/pubhub/pubhubalias.java
@@ -12,6 +12,7 @@ import java.util.StringTokenizer;
 import java.util.TimerTask;
 
 import twcore.bots.PubBotModule;
+import twcore.core.BotSettings;
 import twcore.core.EventRequester;
 import twcore.core.events.InterProcessEvent;
 import twcore.core.events.Message;
@@ -78,6 +79,8 @@ public class pubhubalias extends PubBotModule
 		clearRecordTask = new ClearRecordTask();
 
 		m_botAction.scheduleTaskAtFixedRate(clearRecordTask, CLEAR_DELAY, CLEAR_DELAY);
+		
+		loadWatches();
 	}
 
 	public void requestEvents(EventRequester eventRequester)
@@ -518,6 +521,7 @@ public class pubhubalias extends PubBotModule
 		if( watchedIPs.containsKey( IP ) && (params.length == 1 || params[1] == null || params[1].length() == 0)) {
 			watchedIPs.remove( IP );
 			m_botAction.sendChatMessage( "Login watching disabled for IPs starting with " + IP );
+			saveWatches();
 		} else if(params.length == 1 || params[1] == null || params[1].length() == 0){
 		    m_botAction.sendChatMessage( "Please specify a comment/reason after the IP seperated by a : . For example, !IPWatch 123.123.123.9:Possible hacker .");
 		} else { 
@@ -529,6 +533,7 @@ public class pubhubalias extends PubBotModule
 		        m_botAction.sendChatMessage( "Login watching enabled for IPs starting with " + IP );
 		    }
 			watchedIPs.put( IP, sender + ": "+comment );
+			saveWatches();
 		}
 	}
 
@@ -543,6 +548,7 @@ public class pubhubalias extends PubBotModule
 		if( watchedNames.containsKey( name.toLowerCase() ) && (params.length == 1 || params[1] == null || params[1].length() == 0)) {
 			watchedNames.remove( name.toLowerCase() );
 			m_botAction.sendChatMessage( "Login watching disabled for '" + name + "'." );
+			saveWatches();
 		} else if(params.length == 1 || params[1] == null || params[1].length() == 0) {
 		    m_botAction.sendChatMessage( "Please specify a comment/reason after the name seperated by a : . For example, !NameWatch Pure_Luck:Bad boy .");
 		} else {
@@ -554,6 +560,7 @@ public class pubhubalias extends PubBotModule
 		        m_botAction.sendChatMessage( "Login watching enabled for '" + name + "'." );
 		    }
 			watchedNames.put( name.toLowerCase(), sender + ": "+comment );
+			saveWatches();
 		}
 	}
 
@@ -568,6 +575,7 @@ public class pubhubalias extends PubBotModule
 		if( watchedMIDs.containsKey( MID ) && (params.length == 1 || params[1] == null || params[1].length() == 0)) {
 			watchedMIDs.remove( MID );
 			m_botAction.sendChatMessage( "Login watching disabled for MID: " + MID );
+			saveWatches();
 		} else if(params.length == 1 || params[1] == null || params[1].length() == 0) {
 		    m_botAction.sendChatMessage( "Please specify a comment/reason after the MID seperated by a : . For example, !MIDWatch 777777777:I like the number .");
 		} else {
@@ -579,6 +587,7 @@ public class pubhubalias extends PubBotModule
 		        m_botAction.sendChatMessage( "Login watching enabled for MID: " + MID );
 		    }
 			watchedMIDs.put( MID, sender + ": "+comment );
+			saveWatches();
 			
 		}
 	}
@@ -589,6 +598,7 @@ public class pubhubalias extends PubBotModule
 	public void doClearIPWatchCmd( ) {
 		watchedIPs.clear();
 		m_botAction.sendChatMessage( "All watched IPs cleared." );
+		saveWatches();
 	}
 
 	/**
@@ -597,6 +607,7 @@ public class pubhubalias extends PubBotModule
 	public void doClearNameWatchCmd( ) {
 		watchedNames.clear();
 		m_botAction.sendChatMessage( "All watched names cleared." );
+		saveWatches();
 	}
 
 	/**
@@ -605,6 +616,7 @@ public class pubhubalias extends PubBotModule
 	public void doClearMIDWatchCmd( ) {
 		watchedMIDs.clear();
 		m_botAction.sendChatMessage( "All watched MIDs cleared." );
+		saveWatches();
 	}
 
 	/**
@@ -884,6 +896,123 @@ public class pubhubalias extends PubBotModule
 		{
 			throw new RuntimeException("ERROR: Unable to update alias entry.");
 		}
+	}
+	
+	
+	private void loadWatches() {
+	    BotSettings cfg = m_botAction.getBotSettings();
+        boolean loop = true;
+        int i = 1;
+	    
+	    watchedIPs.clear();
+	    watchedMIDs.clear();
+	    watchedNames.clear();
+	    
+	    // Load the IP watches from the configuration
+	    while(loop) {
+	        String IPWatch = cfg.getString("IPWatch"+i);
+	        if(IPWatch != null && IPWatch.trim().length()>0) {
+	            String[] IPWatchSplit = IPWatch.split(":",2);
+	            if(IPWatchSplit.length == 2)       // Check for corrupted data
+	                watchedIPs.put(IPWatchSplit[0], IPWatchSplit[1]);
+	            i++;
+	        } else {
+	            loop = false;
+	        }
+	    }
+	    
+	    // Load the MID watches from the configuration
+	    loop = true;
+	    i =1;
+	    
+        while(loop) {
+            String MIDWatch = cfg.getString("MIDWatch"+i);
+            if(MIDWatch != null && MIDWatch.trim().length()>0) {
+                String[] MIDWatchSplit = MIDWatch.split(":",2);
+                if(MIDWatchSplit.length == 2)       // Check for corrupted data
+                    watchedMIDs.put(MIDWatchSplit[0], MIDWatchSplit[1]);
+                i++;
+            } else {
+                loop = false;
+            }
+        }
+        
+        // Load the Name watches from the configuration
+        loop = true;
+        i =1;
+        
+        while(loop) {
+            String NameWatch = cfg.getString("NameWatch"+i);
+            if(NameWatch != null && NameWatch.trim().length()>0) {
+                String[] NameWatchSplit = NameWatch.split(":",2);
+                if(NameWatchSplit.length == 2)       // Check for corrupted data
+                    watchedNames.put(NameWatchSplit[0], NameWatchSplit[1]);
+                i++;
+            } else {
+                loop = false;
+            }
+        }
+	    
+        // Done loading watches
+	}
+	
+	private void saveWatches() {
+	    BotSettings cfg = m_botAction.getBotSettings();
+	    boolean loop = true;
+	    int i = 1;
+	    
+	    // Save IP watches
+	    for(String IP:watchedIPs.keySet()) {
+	        cfg.put("IPWatch"+i, IP+":"+watchedIPs.get(IP));
+	        i++;
+	    }
+	    // Clear any other still stored IP watches
+	    while(loop) {
+	        if(cfg.getString("IPWatch"+i) != null) {
+	            cfg.remove("IPWatch"+i);
+	            i++;
+	        } else {
+	            loop = false;
+	        }
+	    }
+	    
+	    i = 1;
+	    loop = true;
+	    
+	    // Save MID watches
+	    for(String MID:watchedMIDs.keySet()) {
+	        cfg.put("MIDWatch"+i, MID+":"+watchedMIDs.get(MID));
+	        i++;
+	    }
+	    // Clear any other still stored MID watches
+	    while(loop) {
+	        if(cfg.getString("MIDWatch"+i) != null) {
+	            cfg.remove("MIDWatch"+i);
+	            i++;
+	        } else {
+	            loop = false;
+	        }
+	    }
+	    
+	    i = 1;
+	    loop = true;
+	    
+	    // Save Name watches
+        for(String Name:watchedNames.keySet()) {
+            cfg.put("NameWatch"+i, Name+":"+watchedNames.get(Name));
+            i++;
+        }
+        // Clear any other still stored MID watches
+        while(loop) {
+            if(cfg.getString("NameWatch"+i) != null) {
+                cfg.remove("NameWatch"+i);
+                i++;
+            } else {
+                loop = false;
+            }
+        }
+        
+        cfg.save();
 	}
 
 	/**
