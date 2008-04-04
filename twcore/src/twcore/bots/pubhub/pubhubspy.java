3147d0a4d606b8e539843adbc86c413aaf59e749
Koen Werdler <koen@werdler.com>
2008-04-04 10:37:35 +0000

    Rebuild of the pubhubspy module: removed methods that weren't needed Added !help menu
    git-svn-id: http://www.twcore.org/svn/trunk@2487 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubhub/pubhubspy.java
+++ twcore/src/twcore/bots/pubhub/pubhubspy.java
@@ -2,8 +2,6 @@ package twcore.bots.pubhub;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 import java.util.TimerTask;
 
 import twcore.bots.PubBotModule;
@@ -31,137 +29,14 @@ public class pubhubspy extends PubBotModule
     ignoreList = new HashMap<String, IgnoreTask>();
     botName = m_botAction.getBotName();
   }
-
-  /**
-   * This method requests the events that are to be used in the module.
-   *
-   * @param eventRequester is the bots EventRequester
-   */
-
-  public void requestEvents(EventRequester eventRequester)
-  {
-    eventRequester.request(EventRequester.MESSAGE);
+  
+  @Override
+  public void requestEvents(EventRequester eventRequester) {
+      eventRequester.request(EventRequester.MESSAGE);
   }
-
-  public void watch(String argString)
-  {
-    String playerName = argString.toLowerCase();
-
-    watchList.add(playerName);
-    m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("watch " + playerName));
-    m_botAction.sendChatMessage("Watching player: " + argString + " enabled.");
-  }
-
-  public void unWatch(String argString)
-  {
-    String playerName = argString.toLowerCase();
-
-    watchList.remove(playerName);
-    m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("unwatch " + playerName));
-    m_botAction.sendChatMessage("Watching player: " + argString + " disabled.");
-  }
-
-  public void doWatchCmd(String argString)
-  {
-    String playerName = argString.toLowerCase();
-
-    if(watchList.contains(playerName))
-      unWatch(argString);
-    else
-      watch(argString);
-  }
-
-  public void doWatchListCmd()
-  {
-    Iterator<String> iterator = watchList.iterator();
-    StringBuffer line = new StringBuffer();
-    String playerName;
-
-    if(watchList.isEmpty())
-      m_botAction.sendChatMessage("Not currently watching any players.");
-
-    for(int counter = 0; iterator.hasNext(); counter++)
-    {
-      playerName = (String) iterator.next();
-      line.append(padString(playerName, 20));
-      if((counter + 1) % 5 == 0)
-      {
-        m_botAction.sendChatMessage(line.toString());
-        line = new StringBuffer();
-      }
-    }
-    m_botAction.sendChatMessage(line.toString());
-  }
-
-  public void doIgnoreCmd(String argString)
-  {
-    String playerName = argString.toLowerCase();
-    if(ignoreList.containsKey(playerName))
-      doIgnoreOff(playerName);
-    else
-      doIgnoreOn(playerName);
-  }
-
-  public void doIgnoreOff(String playerName)
-  {
-    IgnoreTask ignoreTask = ignoreList.get(playerName);
-    m_botAction.cancelTask(ignoreTask);
-    m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("unignore " + playerName));
-    m_botAction.sendChatMessage("Listening to racist words from " + playerName + ".");
-    ignoreList.remove(playerName);
-  }
-
-  public void doIgnoreOn(String playerName)
-  {
-    IgnoreTask ignoreTask = new IgnoreTask(playerName);
-    m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("ignore " + playerName));
-    ignoreList.put(playerName, ignoreTask);
-    m_botAction.scheduleTask(ignoreTask, IGNORE_TIME * 60 * 1000);
-    m_botAction.sendChatMessage("Ignoring " + playerName + " for " + IGNORE_TIME + " minutes.");
-  }
-
-  public void doIgnoreListCmd()
-  {
-    Set<String> set = ignoreList.keySet();
-    Iterator<String> iterator = set.iterator();
-    StringBuffer line = new StringBuffer();
-    String playerName;
-
-    if(ignoreList.isEmpty())
-      m_botAction.sendChatMessage("Not currently ignoring any players.");
-
-    for(int counter = 0; iterator.hasNext(); counter++)
-    {
-      playerName = (String) iterator.next();
-      line.append(padString(playerName, 20));
-      if((counter + 1) % 5 == 0)
-      {
-        m_botAction.sendChatMessage(line.toString());
-        line = new StringBuffer();
-      }
-    }
-    m_botAction.sendChatMessage(line.toString());
-  }
-
-  public void handleChatMessage(String sender, String message)
-  {
-    String command = message.toLowerCase();
-
-    try
-    {
-      if(command.startsWith("!watch "))
-        doWatchCmd(message.substring(7));
-      if(command.equals("!watchlist"))
-        doWatchListCmd();
-      if(command.startsWith("!ignore "))
-        doIgnoreCmd(message.substring(8));
-      if(command.equals("!ignorelist"))
-        doIgnoreListCmd();
-    }
-    catch(Exception e)
-    {
-      m_botAction.sendChatMessage(e.getMessage());
-    }
+  
+  public void cancel() {
+      
   }
 
   public void handleEvent(Message event)
@@ -170,36 +45,78 @@ public class pubhubspy extends PubBotModule
     String message = event.getMessage();
     int messageType = event.getMessageType();
 
-    if(messageType == Message.CHAT_MESSAGE)
-    {
-      handleChatMessage(sender, message);
-    }
-  }
+    if(messageType == Message.CHAT_MESSAGE) {
+        
+        // !help
+        if(message.startsWith("!help ")) {
+            String[] help = { 
+                    "SPY CHAT COMMANDS:",
+                    "!watch <player>                - Relays any chat messages from <player> to chat",
+                    "!watchlist                     - Shows players being !watch'ed",
+                    "!ignore <player>               - Changes ?cheater notification of <player> on racist words to a chat notification only",
+                    "!ignorelist                    - Shows players being !ignore'ed"
+            };
+            m_botAction.smartPrivateMessageSpam(sender, help);
+        }
+        
+        // !watch
+        if(message.startsWith("!watch ")) {
+            String playerName = message.substring(7).toLowerCase();
 
-  public void gotLoadedSpyCmd(String botSender)
-  {
-    Iterator<String> iterator = watchList.iterator();
-    String playerName;
+            if(watchList.contains(playerName)) {
+                // Stop watching the player
+                watchList.remove(playerName);
+                m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("unwatch " + playerName));
+                m_botAction.sendChatMessage("Watching player: " + playerName + " disabled.");
+            } else {
+                // Start watching the player
+                watchList.add(playerName);
+                m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("watch " + playerName));
+                m_botAction.sendChatMessage("Watching player: " + playerName + " enabled.");
+            }
+        }
+        
+        // !watchlist
+        if(message.equals("!watchlist")) {
+            if(watchList.isEmpty())
+                m_botAction.sendChatMessage("Not currently watching any players.");
+            
+            m_botAction.sendChatMessage("Current players on !watch:");
+            for(String watchedPlayer:watchList) {
+                m_botAction.sendChatMessage(" "+watchedPlayer);
+            }
+        }
+        
+        // !ignore
+        if(message.startsWith("!ignore ")) {
+            String playerName = message.substring(8).toLowerCase();
+            
+            if(ignoreList.containsKey(playerName)) {
+                IgnoreTask ignoreTask = ignoreList.get(playerName);
+                m_botAction.cancelTask(ignoreTask);
+                m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("unignore " + playerName));
+                m_botAction.sendChatMessage("Listening to racist words from " + playerName + ".");
+                ignoreList.remove(playerName);
+            } else {
+                m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("ignore " + playerName));
+                IgnoreTask ignoreTask = new IgnoreTask(playerName);
+                ignoreList.put(playerName, ignoreTask);
+                m_botAction.scheduleTask(ignoreTask, IGNORE_TIME * 60 * 1000);
+                m_botAction.sendChatMessage("Ignoring " + playerName + " for " + IGNORE_TIME + " minutes.");
+            }
+        }
+        
+        // !ignorelist
+        if(message.equals("!ignorelist")) {
+            if(ignoreList.isEmpty())
+                m_botAction.sendChatMessage("Not currently ignoring any players.");
 
-    while(iterator.hasNext())
-    {
-      playerName = (String) iterator.next();
-      m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("watch " + playerName.toLowerCase(), botSender));
-    }
-  }
-
-  public void handleIPC(String botSender, String recipient, String sender, String message)
-  {
-    String command = message.toLowerCase();
-
-    try
-    {
-      if(command.equals("loadedspy"))
-        gotLoadedSpyCmd(botSender);
-    }
-    catch(Exception e)
-    {
-      m_botAction.sendChatMessage(e.getMessage());
+            m_botAction.sendChatMessage("Current players on !ignore:");
+            
+            for(String ignoredPlayer:ignoreList.keySet()) {
+                m_botAction.sendChatMessage(ignoredPlayer);
+            }
+        }
     }
   }
 
@@ -219,41 +136,20 @@ public class pubhubspy extends PubBotModule
     IPCMessage ipcMessage = (IPCMessage) event.getObject();
     String message = ipcMessage.getMessage();
     String recipient = ipcMessage.getRecipient();
-    String sender = ipcMessage.getSender();
     String botSender = event.getSenderName();
 
-    if(recipient == null || recipient.equals(botName))
-      handleIPC(botSender, recipient, sender, message);
+    if(recipient == null || recipient.equals(botName)) {
+        
+        if(message.equals("loadedspy")) {
+            // notify the new pubbot of currently watched players
+            for(String watchedPlayer:watchList) {
+                m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("watch " + watchedPlayer, botSender));
+            }
+        }
+    }
   }
 
-  public void cancel()
-  {
-  }
-
-  /**
-   * This method pads the string with whitespace.
-   *
-   * @param string is the string to pad.
-   * @param spaces is the size of the resultant string.
-   * @return the string padded with spaces to fit into spaces characters.  If
-   * the string is too long to fit, it is truncated.
-   */
-
-  private String padString(String string, int spaces)
-  {
-    int whitespaces = spaces - string.length();
-
-    if(whitespaces < 0)
-      return string.substring(spaces);
-
-    StringBuffer stringBuffer = new StringBuffer(string);
-    for(int index = 0; index < whitespaces; index++)
-      stringBuffer.append(' ');
-    return stringBuffer.toString();
-  }
-
-  private class IgnoreTask extends TimerTask
-  {
+  private class IgnoreTask extends TimerTask {
     private String playerName;
 
     public IgnoreTask(String playerName)
@@ -261,10 +157,9 @@ public class pubhubspy extends PubBotModule
       this.playerName = playerName;
     }
 
-    public void run()
-    {
+    public void run() {
       m_botAction.ipcTransmit(getIPCChannel(), new IPCMessage("unignore " + playerName));
-      m_botAction.sendChatMessage("Listening to racist words from " + playerName + ".");
+      m_botAction.sendChatMessage("The !ignore on "+playerName+" expired. Listening to racist words from " + playerName + ".");
     }
   }
 }
