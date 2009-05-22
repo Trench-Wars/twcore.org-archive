f4b03422fd2f4c8c4c71a1a5b3b54b42c1be0b57
Koen Werdler <koen@werdler.com>
2009-05-22 19:24:32 +0000

    Added the ability of Staffbot to ignore certain players for Server Errors. The players to be ignored can be set by a SMod or higher.
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@3318 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/staffbot/staffbot_serverwarningecho.java
+++ src/twcore/bots/staffbot/staffbot_serverwarningecho.java
@@ -1,10 +1,21 @@
 package twcore.bots.staffbot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import twcore.bots.Module;
 import twcore.core.EventRequester;
 import twcore.core.events.Message;
 
 public class staffbot_serverwarningecho extends Module {
+    
+    List<String> ignoredPlayers = new ArrayList<String>();
+    
+    final String[] helpSmod = {
+            "----------------[ ServerErrorEcho: SMod+ ]-----------------",
+            " !errorignore <player>     - Ignores server errors from <player> (toggable).",
+            " !ignorelist               - Shows ignored players for server errors."
+    };
 
 	@Override
 	public void requestEvents(EventRequester eventRequester) {
@@ -12,10 +23,52 @@ public class staffbot_serverwarningecho extends Module {
 	}
 	
 	public void handleEvent(Message event) {
+
+ 	    if(event.getMessageType() == Message.REMOTE_PRIVATE_MESSAGE || event.getMessageType() == Message.PRIVATE_MESSAGE) {
+	        String message = event.getMessage().toLowerCase();
+	        String name = event.getMessager() == null ? m_botAction.getPlayerName(event.getPlayerID()) : event.getMessager();
+	        
+	        if(!m_botAction.getOperatorList().isSmod(name)) {
+	            return;
+	        }
+	        
+	        if(message.startsWith("!help")) {
+	            m_botAction.smartPrivateMessageSpam(name, helpSmod);
+	        } else
+	        
+	        if(message.startsWith("!errorignore ")) {
+	            String player = message.substring(12).trim();
+	            if(player.length() == 0) {
+	                m_botAction.sendSmartPrivateMessage(name, "Syntax error. Please use !errorignore <name>. PM !help for more information.");
+	            } else {
+	                if(ignoredPlayers.contains(player)) {
+	                    ignoredPlayers.remove(player);
+	                    m_botAction.sendSmartPrivateMessage(name, "Player '"+player+"' removed from ignore list.");
+	                    m_botAction.sendChatMessage(2, name + " removed the ignore of '"+player+"' for server errors.");
+	                } else {
+	                    ignoredPlayers.add(player);
+	                    m_botAction.sendSmartPrivateMessage(name, "Player '"+player+"' added to ignore list.");
+	                    m_botAction.sendChatMessage(2, name + " ignored '"+player+"' for server errors.");
+	                }
+	            }
+	        } else
+	        if(message.startsWith("!ignorelist")) {
+	            if(ignoredPlayers.isEmpty()) {
+	                m_botAction.sendSmartPrivateMessage(name, "No players currently ignored.");
+	            } else {
+	                m_botAction.sendSmartPrivateMessage(name, "Ignored players:");
+	                for(String player : ignoredPlayers) {
+	                    m_botAction.sendSmartPrivateMessage(name, "- "+player);
+	                }
+	            }
+	        }
+	    }
 		if(event.getMessageType() == Message.SERVER_ERROR) {
 		    // 1 = staff chat
 	        // 2 = smod chat
-			m_botAction.sendChatMessage(2, getMessageTypeString(event.getMessageType()) + ": " + event.getMessage());
+		    if(ignoredPlayers.isEmpty() || !ignoredPlayers.contains(getPlayerNameFromError(event.getMessage()).toLowerCase()))
+		        m_botAction.sendChatMessage(2, getMessageTypeString(event.getMessageType()) + ": " + event.getMessage());
+
 		}
 	}
 	
@@ -51,10 +104,23 @@ public class staffbot_serverwarningecho extends Module {
 	    }
 	    return "Other";
 	  }
+	  
+	  private String getPlayerNameFromError(String errorMessage) {
+	      if(errorMessage.indexOf('(') != -1 && errorMessage.indexOf(')') != -1) {
+    	      int startPosition = errorMessage.indexOf('(')+1;
+    	      int endPosition = errorMessage.indexOf(')');
+    	      return errorMessage.substring(startPosition, endPosition);
+	      } else {
+	          return null;
+	      }
+	  }
 
 	@Override
-	public void cancel() {}
+	public void cancel() {
+	    ignoredPlayers.clear();
+	}
 
 	@Override
-	public void initializeModule() {}
+	public void initializeModule() {
+	}
 }
