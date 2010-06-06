ea1917290c5d4a08170c3c09570068c6a4fa6d3b
Dexter <andreviniciusandre@gmail.com>
2010-06-06 19:00:45 +0000

    chat system updates
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@3749 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/hockeybot/hockeymediator/GameRequest.java
+++ src/twcore/bots/hockeybot/hockeymediator/GameRequest.java
@@ -6,10 +6,20 @@ public class GameRequest {
     String sqdChallenged;
     String requester;
     
-    public GameRequest(String requester, String sqdChallenger, String sqdChallenged){
+    int idSqdChallenger;
+    int idSqdChallenged;
+    
+    String chatSqdChallenger, chatSqdChallenged;
+    
+    public GameRequest(String requester, String sqdChallenger, String sqdChallenged, int idSqdChallenger, int idSqdChallenged,
+            String chatSqdChallenger, String chatSqdChallenged){
         this.sqdChallenged = sqdChallenged;
         this.sqdChallenger = sqdChallenger;
         this.requester = requester;
+        this.idSqdChallenger = idSqdChallenger;
+        this.idSqdChallenged = idSqdChallenged;
+        this.chatSqdChallenger = chatSqdChallenger;
+        this.chatSqdChallenged = chatSqdChallenged;
     }
 
     public String getSqdChallenger() {
@@ -24,6 +34,22 @@ public class GameRequest {
         return sqdChallenged;
     }
 
+    public String getChatSqdChallenger() {
+        return chatSqdChallenger;
+    }
+
+    public void setChatSqdChallenger(String chatSqdChallenger) {
+        this.chatSqdChallenger = chatSqdChallenger;
+    }
+
+    public String getChatSqdChallenged() {
+        return chatSqdChallenged;
+    }
+
+    public void setChatSqdChallenged(String chatSqdChallenged) {
+        this.chatSqdChallenged = chatSqdChallenged;
+    }
+
     public void setSqdChallenged(String sqdChallenged) {
         this.sqdChallenged = sqdChallenged;
     }
@@ -36,5 +62,21 @@ public class GameRequest {
         this.requester = requester;
     }
     
+    public int getIdsqdChallenger() {
+        return idSqdChallenger;
+    }
+
+    public void setIdsqdChallenger(int idsqdChallenger) {
+        this.idSqdChallenger = idsqdChallenger;
+    }
+
+    public int getIdSqdChallenged() {
+        return idSqdChallenged;
+    }
+
+    public void setIdSqdChallenged(int idSqdChallenged) {
+        this.idSqdChallenged = idSqdChallenged;
+    }
+
     
 }
