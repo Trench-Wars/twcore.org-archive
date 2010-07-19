1d367d0924b0c9d92b89f49caa10d3e6ca213d6b
Arobas <uberleetcoder@gmail.com>
2010-07-19 22:56:33 +0000

    !reloadconf <bot type> on the core. The bot needs to implement Reloadable, else it won't do anything. Currently testing this feature on MatchBot
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@3837 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/matchbot/matchbot.java
+++ src/twcore/bots/matchbot/matchbot.java
@@ -13,6 +13,7 @@ import twcore.core.BotAction;
 import twcore.core.BotSettings;
 import twcore.core.EventRequester;
 import twcore.core.OperatorList;
+import twcore.core.Reloadable;
 import twcore.core.SubspaceBot;
 import twcore.core.events.ArenaJoined;
 import twcore.core.events.BallPosition;
@@ -42,7 +43,7 @@ import twcore.core.util.Tools;
 /**
  * Runs automated squad vs. squad TWD matches.
  */
-public class matchbot extends SubspaceBot
+public class matchbot extends SubspaceBot implements Reloadable
 {
 
     MatchGame m_game;
@@ -1258,6 +1259,10 @@ public class matchbot extends SubspaceBot
         m_botAction.cancelTasks();
         m_botAction.ipcUnSubscribe("MatchBot");
     }
+
+	public void reload() {
+		m_botSettings.reloadFile();
+	}
 }
 
 
