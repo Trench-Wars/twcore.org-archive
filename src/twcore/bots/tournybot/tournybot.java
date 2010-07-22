a2ec7c793f07172ea776645085659335b87e204b
Dexter <andreviniciusandre@gmail.com>
2010-07-22 01:16:26 +0000

    testing to money tourny
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@3853 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/tournybot/tournybot.java
+++ src/twcore/bots/tournybot/tournybot.java
@@ -985,7 +985,7 @@ public class tournybot extends SubspaceBot {
 		    return 512;
 		}
 
-		pStats tPos = players.get( name );
+		pStats tPos = players.get( name );    
 		fStats pPos = freqs.get( tPos.getFreq() );
 
 		int nroer = pPos.getPlayerNro();
@@ -1005,11 +1005,14 @@ public class tournybot extends SubspaceBot {
 		int x;
 
 		// If it is a base-tournament, the boxes on the right are used.
-		if (base) {
+		
+		x = x2 + (127 * box1);
+	        
+		/*if (base) {
 			x = x2 + (127 * box1) + 517;
 		} else {
 			x = x2 + (127 * box1);
-		}
+		}*/
 
 		if (tPos.getPlayerState() == 0) {
 
@@ -1024,7 +1027,7 @@ public class tournybot extends SubspaceBot {
 
 
 	/*
-	 * Calculates the X coord for name based on the 2 coordinates in .cfg
+	 * Calculates the Y coord for name based on the 2 coordinates in .cfg
 	 */
 
 	public int playerYPos(String name) {
@@ -1032,7 +1035,7 @@ public class tournybot extends SubspaceBot {
 		if (!playerStillIn( name )) {
 		    return 512;
 		}
-
+		                                                                                                                                                                                                                                                                                                                        
 		pStats tPos = players.get( name );
 		fStats pPos = freqs.get( tPos.getFreq() );
 
@@ -1265,7 +1268,7 @@ public class tournybot extends SubspaceBot {
 					m_botAction.sendArenaMessage("Registrations locked! Skipping first round due to lack of players.", 2);
 				} else {
 					firstRound = 1;
-					maxBox = 32;
+					maxBox = 64;//32;
 					m_botAction.sendArenaMessage("Registartions locked!", 2);
 				}
 
