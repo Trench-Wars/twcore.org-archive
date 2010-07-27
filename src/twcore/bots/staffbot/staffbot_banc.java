46f35c896ad0e914edc70a6c423376ced77a6863
Dexter <andreviniciusandre@gmail.com>
2010-07-27 18:01:14 +0000

    to check first if doesn't have d
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@3907 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/staffbot/staffbot_banc.java
+++ src/twcore/bots/staffbot/staffbot_banc.java
@@ -500,7 +500,7 @@ public class staffbot_banc extends Module {
 			return;
 		}
 		
-		if(!Tools.isAllDigits(timeStr) || !timeStr.contains("d")) {
+		if(!timeStr.contains("d") || !Tools.isAllDigits(timeStr) ) {
 			m_botAction.sendSmartPrivateMessage(name, "Syntax error. Please specify <playername>:<time/mins> or PM !help for more information.");
 			return;
 		} else if(timeStr.length() > 6) {
