218ca6dae52f3cf9bea73124566cded623f4ef82
Arobas <uberleetcoder@gmail.com>
2010-08-13 00:42:01 +0000

    trying to fix a bug
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@3950 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubbot/pubbotmessage.java
+++ src/twcore/bots/pubbot/pubbotmessage.java
@@ -6,151 +6,104 @@ import java.util.TimerTask;
 
 import twcore.bots.PubBotModule;
 import twcore.core.EventRequester;
-import twcore.core.events.InterProcessEvent;
 import twcore.core.events.PlayerEntered;
 import twcore.core.util.ipc.IPCMessage;
 
-public class pubbotmessage extends PubBotModule
-{
-  private String botName;
-  Queue<String> checkQueue;
-  Timer queueTimer;
-  private long CHECKTIME = 2; // Time in seconds how fast names are passed to MessageBot
-  private long START_IDLE = 10; // Time in seconds how long bot waits before passing names to MessageBot after module loaded
-  queueTimerTask tTask;
+public class pubbotmessage extends PubBotModule {
+	
+	private String botName;
+	private Queue<String> checkQueue;
+	Timer queueTimer;
+	private long CHECKTIME = 2; // Time in seconds how fast names are passed to
+								// MessageBot
+	private long START_IDLE = 10; // Time in seconds how long bot waits before
+									// passing names to MessageBot after module
+									// loaded
+	private queueTimerTask tTask;
 
-  public void initializeModule()
-  {
-  	checkQueue = new Queue<String>();
-    botName = m_botAction.getBotName();
-    queueTimer = new Timer();
-    queueTimer.schedule(tTask, START_IDLE * 1000);
-  }
+	public void initializeModule() {
+		checkQueue = new Queue<String>();
+		botName = m_botAction.getBotName();
+		queueTimer = new Timer();
+		queueTimer.schedule(tTask, START_IDLE * 1000);
+	}
 
-  public void requestEvents(EventRequester eventRequester)
-  {
-  //  eventRequester.request(EventRequester.MESSAGE);
-    eventRequester.request(EventRequester.PLAYER_ENTERED);
-  }
-/*
-  public void handleEvent(Message event)
-  {
-    String message = event.getMessage();
-    int messageType = event.getMessageType();
+	public void requestEvents(EventRequester eventRequester) {
+		// eventRequester.request(EventRequester.MESSAGE);
+		eventRequester.request(EventRequester.PLAYER_ENTERED);
+	}
 
-    if(messageType == Message.ARENA_MESSAGE)
-    	if(message.toLowerCase().startsWith("time:"))
-    	{
-    		String name = checkQueue.next();
-    		if(name == null) return; 
-    		String pieces[] = message.split(":");
-    		int mins = Integer.parseInt(pieces[3]);
-    		if(mins < 1)
-    		//	m_botAction.sendSmartPrivateMessage("MessageBot", "!login " + name);
-    			m_botAction.ipcTransmit("messages", new IPCMessage(name, "MessageBot"));
-    	}
-  }
-*/
+	/*
+	public void handleEvent(Message event) {
+		
+		String message = event.getMessage();
+		int messageType = event.getMessageType();
 
-  public void gotNotRecordedCmd(String argString)
-  {
-  	checkQueue.add(argString.toLowerCase());
-  }
+		if (messageType == Message.ARENA_MESSAGE)
+			if (message.toLowerCase().startsWith("time:")) {
+				String name = checkQueue.next();
+				if (name == null)
+					return;
+				String pieces[] = message.split(":");
+				int mins = Integer.parseInt(pieces[3]);
+				if (mins < 1)
+					// m_botAction.sendSmartPrivateMessage("MessageBot",
+					// "!login " + name);
+					m_botAction.ipcTransmit("messages", new IPCMessage(name, "MessageBot"));
+			}
+	}
+	*/
 
-  public void handleBotIPC(String botSender, String recipient, String sender, String message)
-  {
-    String command = message.toLowerCase();
+	public void handleEvent(PlayerEntered event) {
+		String name = m_botAction.getPlayerName(event.getPlayerID()).toLowerCase();
+		if (name != null)
+			checkQueue.add(name);
+	}
 
-    try
-    {
-      if(command.startsWith("notrecorded "))
-        gotNotRecordedCmd(message.substring(12));
-    }
-    catch(Exception e)
-    {
-      m_botAction.sendChatMessage(e.getMessage());
-    }
-  }
+	private class queueTimerTask extends TimerTask {
+		public void run() {
+			String name = checkQueue.next();
+			if (name == null) {
+				queueTimer.schedule(tTask, CHECKTIME * 1000);
+				return;
+			}
+			m_botAction.ipcTransmit("messages", new IPCMessage(name, "MessageBot"));
+			queueTimer.schedule(tTask, CHECKTIME * 1000);
+		}
+	}
+	
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private class Queue<T> {
+		
+		ArrayList<T> objects;
 
-  /**
-   * This method handles an InterProcessEvent.
-   *
-   * @param event is the InterProcessEvent to handle.
-   */
+		public Queue() {
+			objects = new ArrayList<T>();
+		}
 
-  public void handleEvent(InterProcessEvent event)
-  {
-	  // If the event.getObject() is anything else then the IPCMessage (pubbotchatIPC f.ex) then return
-	  if(event.getObject() instanceof IPCMessage == false) {
-		  return;
-	  }
-    IPCMessage ipcMessage = (IPCMessage) event.getObject();
-    String message = ipcMessage.getMessage();
-    String recipient = ipcMessage.getRecipient();
-    String sender = ipcMessage.getSender();
-    String botSender = event.getSenderName();
+		public T next() {
+			T obj = null;
 
-    try
-    {
-      if(recipient == null || recipient.equals(botName))
-      {
-        if(sender == null)
-          handleBotIPC(botSender, recipient, sender, message);
-      }
-    }
-    catch(Exception e)
-    {
-      m_botAction.sendChatMessage(e.getMessage());
-    }
-  }
-  public void handleEvent(PlayerEntered event){
-      String name = m_botAction.getPlayerName(event.getPlayerID()).toLowerCase();
-      if(name == null)
-          checkQueue.add(name);
-      }
-  public void cancel()
-  {
-  }
-  private class queueTimerTask extends TimerTask {
-      public void run() {
-          String name = checkQueue.next();
-          if(name == null){
-              queueTimer.schedule(tTask, CHECKTIME * 1000);
-              return; 
-          }
-          m_botAction.ipcTransmit("messages", new IPCMessage(name, "MessageBot"));
-          queueTimer.schedule(tTask, CHECKTIME * 1000);
-      }
-  }
+			try {
+				obj = objects.remove(0);
+			} catch (IndexOutOfBoundsException ioobe) {
+			}
+			return obj;
+		}
+
+		public void add(T obj) {
+			objects.add(obj);
+		}
+
+		public int size() {
+			return objects.size();
+		}
+	}
+
 }
 
-class Queue<T>
-{
-	ArrayList<T> objects;
-
-	public Queue()
-	{
-		objects = new ArrayList<T>();
-	}
-
-	public T next()
-	{
-	    T obj = null;
-
-	    try {
-	        obj = objects.get(0);
-	        objects.remove(0);
-	    } catch(IndexOutOfBoundsException ioobe) {}
-		return obj;
-	}
-
-	public void add(T obj)
-	{
-		objects.add(obj);
-	}
-
-	public int size()
-	{
-		return objects.size();
-	}
-}
