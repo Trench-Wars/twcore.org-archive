e9c4d871b6145ce11681bbee78261227024c9155
Arobas <uberleetcoder@gmail.com>
2010-08-16 23:05:14 +0000

    git-svn-id: http://www.twcore.org/svn/trunk@3961 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/staffbot/staffbot_staffchat_savelog.java
@@ -0,0 +1,143 @@
+package twcore.bots.staffbot;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.TimerTask;
+import java.util.Vector;
+
+import twcore.bots.Module;
+import twcore.core.BotSettings;
+import twcore.core.EventRequester;
+import twcore.core.OperatorList;
+import twcore.core.events.Message;
+import twcore.core.util.Tools;
+
+/**
+ * This module reports bad commands (and player kicks for message flooding) that are shown on the *log
+ * This module also logs all *log lines to a file. The functionality is copied from the mrarrogant bot.
+ * 
+ * @author MMaverick
+ */
+public class staffbot_staffchat_savelog extends Module {
+    
+    public static final int CHECK_LOG_TIME = 5 * Tools.TimeInMillis.MINUTE;
+
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.US);
+
+    private FileWriter logFile;
+    private File path;
+    private File currentFile;
+    private StringBuffer buffer;
+    
+    // TimerTasks
+    private CheckStaffChat checkStaffChat = new CheckStaffChat();
+    private WriteToLog writeToLog = new WriteToLog();
+    
+
+    @Override
+    public void initializeModule() {
+    	
+        BotSettings botSettings = m_botAction.getBotSettings();
+        path = new File(botSettings.getString("logpath_staffchat"));
+        buffer = new StringBuffer();
+        
+    	SimpleDateFormat fileNameFormat = new SimpleDateFormat("yyyymmdd");
+        currentFile = new File(path, fileNameFormat.format(new Date()+".log"));
+        try {
+			logFile = new FileWriter(currentFile, true);
+		} catch (IOException e) {
+			Tools.printStackTrace(e);
+			cancel();
+		}
+        
+        // Algo to have time looking like 00:05, 00:10, etc..
+        // We don't want a log with weird time like 00:03, 00:07..
+        // We must synch our timerTasks!
+        SimpleDateFormat format = new SimpleDateFormat("mm:ss", Locale.US);
+        String[] split = format.format(new Date()).split(":");
+        int min = Integer.parseInt(split[0])%5;
+        int sec = Integer.parseInt(split[1]);
+        
+        int diff = 5*60-(min+1)*60+60-sec;
+        if (diff<=10)
+        	diff += 5*60;
+        
+        // Must be executed 10 seconds before "writeToLog"
+        m_botAction.scheduleTaskAtFixedRate(checkStaffChat, diff-10, CHECK_LOG_TIME);
+        m_botAction.scheduleTaskAtFixedRate(writeToLog, diff, CHECK_LOG_TIME);
+    }
+
+	@Override
+	public void requestEvents(EventRequester eventRequester) {
+		eventRequester.request(EventRequester.MESSAGE);
+	}
+
+	
+	public void handleEvent(Message event) {
+
+		StringBuilder builder = new StringBuilder();
+		String line = event.getMessage();
+        if( event.getMessageType() == Message.ARENA_MESSAGE ) {
+        	if (line.startsWith("(staff) staff: ")) {
+        		builder.append(",");
+        		builder.append(line.substring(15));
+        	}
+        }        
+	}
+	
+    @Override
+    public void cancel() {
+        try {
+        	if (logFile!=null)
+        		logFile.close();
+        } catch(IOException ioe) {}
+        
+        m_botAction.cancelTask(checkStaffChat);
+        m_botAction.cancelTask(writeToLog);
+    }
+	  
+
+	private class CheckStaffChat extends TimerTask {
+        public void run() {
+            m_botAction.sendUnfilteredPublicMessage("?chat");
+        }
+    }
+	
+	private class WriteToLog extends TimerTask {
+	    
+        public void run() {
+            try {
+
+            	SimpleDateFormat fileNameFormat = new SimpleDateFormat("yyyymmdd");
+                File newFile = new File(path, fileNameFormat.format(new Date()+".log"));
+            	
+                if (!currentFile.getAbsolutePath().equals(newFile.getAbsolutePath())) {
+                	
+                	currentFile = newFile;
+                    logFile.close();
+                    
+                    // Open new file
+                    logFile = new FileWriter(currentFile, true);
+                }
+                
+                String line = dateFormat.format(new Date()) + " : ";
+                
+                logFile.write(line+buffer.toString().substring(1) + '\n');
+                logFile.flush();
+                buffer = new StringBuffer();
+                
+            } catch (IOException e) {
+                Tools.printStackTrace("IOException encountered while saving command to log file", e);
+                m_botAction.sendChatMessage("IOException encountered while saving command to log file: " + e.getMessage());
+            }
+        }
+    }
+
+}
