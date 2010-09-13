d04e43119a1c94de963e44ac3ad5976502b3da7f
Arobas <uberleetcoder@gmail.com>
2010-09-13 04:48:04 +0000

    skeleton needed in /pubhub when you add a module in /pubbot
    git-svn-id: http://www.twcore.org/svn/trunk@4016 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/pubhub/pubhubgatekeeper.java
@@ -0,0 +1,27 @@
+package twcore.bots.pubhub;
+
+import twcore.bots.PubBotModule;
+import twcore.core.EventRequester;
+
+public class pubhubgatekeeper extends PubBotModule {
+
+    /**
+     * This method initializes the pubhubtk.java module.  It is called after
+     * m_botAction has been initialized.
+     */
+    public void initializeModule() {
+    }
+
+
+    /**
+     * Requests the events.  No events.
+     */
+    public void requestEvents(EventRequester eventRequester) {
+    }
+
+    /**
+     * Unimplemented.
+     */
+    public void cancel() {
+    }
+}
