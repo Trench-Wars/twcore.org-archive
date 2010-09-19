3f3d2a31236688182c6f01018d4e69e3bf78905f
Arobas <uberleetcoder@gmail.com>
2010-09-19 05:08:49 +0000

    new package for purepubbot new option !settile some minor update
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@4026 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ src/twcore/bots/pubsystem/PubException.java
@@ -0,0 +1,15 @@
+package twcore.bots.pubsystem;
+
+public class PubException extends Exception {
+
+	private String message;
+	
+	public PubException(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+}
---
+++ src/twcore/bots/pubsystem/PubStore.java
@@ -0,0 +1,68 @@
+package twcore.bots.pubsystem;
+
+import java.util.LinkedHashMap;
+
+import twcore.bots.pubsystem.item.PubItem;
+import twcore.bots.pubsystem.item.PubItemRestriction;
+
+public class PubStore {
+
+    private LinkedHashMap<String, PubItem> items;
+    
+    private boolean opened = true;
+
+    public PubStore(){
+        this.items = new LinkedHashMap<String, PubItem>();
+    }
+    
+    public PubItem buy(String itemName, PubPlayer player, int shipType) throws PubException {
+
+    	if (!opened)
+    		throw new PubException("The store is closed!");
+    	
+        PubItem item = items.get(itemName);
+
+        if (item == null)
+        	throw new PubException("This item does not exist.");
+        
+        if (item.isRestricted()) {
+        	PubItemRestriction restriction = item.getRestriction();
+        	restriction.check(item, player, shipType);
+        }
+
+        if (player.getMoney() < item.getPrice())
+        	throw new PubException("You do not have enough money to buy this item.");
+        
+        if (item != null) {
+	        player.setMoney(player.getMoney() - item.getPrice());
+	        player.addItem(item);
+        }
+        
+        item.hasBeenBought();
+
+        return item;
+    }
+    
+    public void addItem(PubItem item, String itemName) {
+    	items.put(itemName, item);
+    	for(String abbv: item.getAbbreviations()) {
+    		items.put(abbv, item);
+    	}
+    }
+    
+    public LinkedHashMap<String, PubItem> getItems() {
+    	return items;
+    }
+    
+    public void turnOn() {
+    	this.opened = true;
+    }
+    
+    public void turnOff() {
+    	this.opened = false;
+    }
+    
+    public boolean isOpened() {
+    	return opened;
+    }
+}
