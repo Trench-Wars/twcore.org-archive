d05eb6416a991bfea8752da53d5648fca83ac881
qui <qui>
2010-10-22 14:33:25 +0000

    testing #539
    git-svn-id: http://www.twcore.org/svn/trunk@4196 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubbot/pubbotbanc.java
+++ twcore/src/twcore/bots/pubbot/pubbotbanc.java
@@ -116,7 +116,8 @@ public class pubbotbanc extends PubBotModule {
         try{
             String namePlayer = m_botAction.getPlayerName(event.getPlayerID());
             m_botAction.sendPrivateMessage("quiles", "Someone changed ship: "+namePlayer);
-            if(banCSuperLocked.contains(namePlayer.toLowerCase()) && ( event.getShipType() == 2 || event.getShipType() == 4 || event.getShipType() == 8 )){
+            
+            if( banCSuperLocked.contains( namePlayer.toLowerCase() ) ){// && ( event.getShipType() == 2 || event.getShipType() == 4 || event.getShipType() == 8 )){
                 superLockMethod(namePlayer, event.getShipType());
                 m_botAction.sendPrivateMessage("quiles", "List contains "+namePlayer);
             }
