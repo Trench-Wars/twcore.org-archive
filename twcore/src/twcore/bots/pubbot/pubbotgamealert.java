5c1b48dfe8f63f37809ccb9c5d509ddbb742ba7f
WingZero <wingzerotw@gmail.com>
2010-11-10 08:05:32 +0000

    Finalized version: removed debugger and also made ipc's more specific
    git-svn-id: http://www.twcore.org/svn/trunk@4312 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubbot/pubbotgamealert.java
+++ twcore/src/twcore/bots/pubbot/pubbotgamealert.java
@@ -3,7 +3,6 @@ package twcore.bots.pubbot;
 import twcore.bots.PubBotModule;
 import twcore.core.EventRequester;
 import twcore.core.events.InterProcessEvent;
-import twcore.core.events.Message;
 import twcore.core.events.PlayerEntered;
 import twcore.core.game.Player;
 import twcore.core.util.ipc.IPCMessage;
@@ -15,7 +14,6 @@ import twcore.core.util.ipc.IPCMessage;
 public class pubbotgamealert extends PubBotModule {
 
     String botName;
-    boolean debug = false;
     
     @Override
     public void initializeModule() {
@@ -24,18 +22,15 @@ public class pubbotgamealert extends PubBotModule {
 
     @Override
     public void cancel() {
-        
     }
 
     @Override
     public void requestEvents( EventRequester eventRequester ){
         eventRequester.request( EventRequester.PLAYER_ENTERED );
-        eventRequester.request( EventRequester.MESSAGE );
     }
 
     /**
      * This method handles an InterProcess event.
-     *
      * @param event is the IPC event to handle.
      */
     public void handleEvent(InterProcessEvent event)
@@ -46,61 +41,19 @@ public class pubbotgamealert extends PubBotModule {
 
         IPCMessage ipcMessage = (IPCMessage) event.getObject();
         String message = ipcMessage.getMessage();
-        
-        if (debug)
-            relayMessage(message);
 
-        try {
-            if(message.startsWith("send "))
-                sendAlert(message.substring(message.indexOf(' ')+1, message.indexOf(':')), message.substring(message.indexOf(':')+1));
-        }
-        catch(Exception e) {
-            m_botAction.sendChatMessage(e.getMessage());
-        }
+        if(message.startsWith("send "))
+            sendAlert(message.substring(message.indexOf(' ')+1, message.indexOf(':')), message.substring(message.indexOf(':')+1));
     }
-    
     
     public void handleEvent(PlayerEntered event) {
         String name = m_botAction.getPlayerName(event.getPlayerID());
         Player p = m_botAction.getPlayer(name);
         String squad = p.getSquadName();
-        m_botAction.ipcSendMessage(getIPCChannel(), "player " + name + ":" + squad, null, botName);
-    }
-    
-    public void handleEvent(Message event) {
-        int type = event.getMessageType();
-        String msg = event.getMessage();
-        String name = getSender(event);
-        if ((type == Message.PRIVATE_MESSAGE || type == Message.REMOTE_PRIVATE_MESSAGE) && "WingZero".equalsIgnoreCase(name) && msg.equalsIgnoreCase(msg))
-            setDebug(name);
-    }
-    
-    public void setDebug(String name) {
-        if (!debug) {
-            debug = true;
-            m_botAction.sendSmartPrivateMessage(name, "Debug mode enabled.");
-        }
-        else {
-            debug = false;
-            m_botAction.sendSmartPrivateMessage(name, "Debug mode disabled.");
-        }
-    }
-    
-    public void relayMessage(String msg) {
-        m_botAction.sendSmartPrivateMessage("WingZero", msg);
+        m_botAction.ipcSendMessage(this.getIPCChannel(), "player " + name + ":" + squad, this.getPubHubName(), botName);
     }
     
     public void sendAlert(String name, String msg) {
         m_botAction.sendSmartPrivateMessage(name, msg);
-    }
-
-    private String getSender(Message event)
-    {
-        int messageType = event.getMessageType();
-
-        if(messageType == Message.REMOTE_PRIVATE_MESSAGE || messageType == Message.CHAT_MESSAGE)
-            return event.getMessager();
-        int senderID = event.getPlayerID();
-        return m_botAction.getPlayerName(senderID);
     }
 }
