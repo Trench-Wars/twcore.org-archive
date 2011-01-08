0e8467089184a1217a97078340d2c8a3047f82d7
Derek Marr <derekmarr10@hotmail.com>
2011-01-08 13:23:36 +0000

    There is no second chat for pubbot's.
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@4521 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubbot/pubbotgatekeeper.java
+++ src/twcore/bots/pubbot/pubbotgatekeeper.java
@@ -34,7 +34,7 @@ public class pubbotgatekeeper extends PubBotModule {
 			// Received from a *info
 			if (message.startsWith("Ping:0ms"))
 			{
-				m_botAction.sendChatMessage(2, "INTER-ZONE: " + lastPlayer + " (" + m_botAction.getArenaName() + ")");
+				m_botAction.sendChatMessage("INTER-ZONE: " + lastPlayer + " (" + m_botAction.getArenaName() + ")");
 			}
 		}
 	
