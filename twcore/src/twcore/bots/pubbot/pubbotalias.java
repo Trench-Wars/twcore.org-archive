7fa6b3db82b879bff24f7bfd647da77cb13955af
WingZero <wingzerotw@gmail.com>
2011-01-17 13:22:40 +0000

    last attempt
    git-svn-id: http://www.twcore.org/svn/trunk@4591 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubbot/pubbotalias.java
+++ twcore/src/twcore/bots/pubbot/pubbotalias.java
@@ -1,65 +1,94 @@
 package twcore.bots.pubbot;
 
+import java.util.HashSet;
+
 import twcore.bots.PubBotModule;
 import twcore.core.EventRequester;
+import twcore.core.events.FrequencyShipChange;
+import twcore.core.events.InterProcessEvent;
 import twcore.core.events.Message;
 import twcore.core.events.PlayerEntered;
+import twcore.core.util.ipc.IPCMessage;
 
 public class pubbotalias extends PubBotModule {
 
-  public void initializeModule() {}
-
-  public void requestEvents(EventRequester eventRequester)
-  {
-    eventRequester.request(EventRequester.MESSAGE);
-    eventRequester.request(EventRequester.PLAYER_ENTERED);
-  }
-
-  public void sendPlayerInfo(String message)
-  {
-    String playerName = getInfo(message, "TypedName:");
-    String playerIP = getInfo(message, "IP:");
-    String playerMacID = getInfo(message, "MachineId:");
-
-    m_botAction.ipcSendMessage(getIPCChannel(), "info " + playerName + ":" + playerIP + ":" + playerMacID, getPubHubName(), "pubbotalias");
-  }
-
-  public void handleArenaMessage(String message)
-  {
-    if(message.startsWith("IP:"))
-      sendPlayerInfo(message);
-  }
-
-  public void handleEvent(Message event)
-  {
-    String message = event.getMessage();
-    int messageType = event.getMessageType();
-
-    if(messageType == Message.ARENA_MESSAGE)
-    	handleArenaMessage(message);
-  }
-
-  public void handleEvent(PlayerEntered event)
-  {
-    String playerName = event.getPlayerName();
-    if(playerName.startsWith("^") == false) {
-    	m_botAction.sendUnfilteredPrivateMessage(playerName, "*info");
+    HashSet<String> specs = new HashSet<String>();
+    
+    public void initializeModule() {
+        m_botAction.ipcSubscribe("banc");
     }
-  }
 
-  public void cancel() {}
+    public void requestEvents(EventRequester eventRequester) {
+        eventRequester.request(EventRequester.MESSAGE);
+        eventRequester.request(EventRequester.PLAYER_ENTERED);
+        eventRequester.request(EventRequester.FREQUENCY_SHIP_CHANGE);
+    }
+    
+    public void handleEvent(InterProcessEvent event) {
+        if (event.getChannel().equals("banc")) {
+            IPCMessage ipc = (IPCMessage)event.getObject();
+            String msg = ipc.getMessage();
+            String name;
+            if (msg.startsWith("addspec")) {
+                name = msg.substring(msg.indexOf(" ") + 1);
+                if (!specs.contains(name.toLowerCase()))
+                    specs.add(name.toLowerCase());
+            } else if (msg.startsWith("remspec")) {
+                name = msg.substring(msg.indexOf(" ") + 1);
+                if (specs.contains(name.toLowerCase()))
+                    specs.remove(name.toLowerCase());
+            }
+        }
+    }
 
-  private String getInfo(String message, String infoName)
-  {
-    int beginIndex = message.indexOf(infoName);
-    int endIndex;
+    public void sendPlayerInfo(String message) {
+        String playerName = getInfo(message, "TypedName:");
+        String playerIP = getInfo(message, "IP:");
+        String playerMacID = getInfo(message, "MachineId:");
 
-    if(beginIndex == -1)
-      return null;
-    beginIndex = beginIndex + infoName.length();
-    endIndex = message.indexOf("  ", beginIndex);
-    if(endIndex == -1)
-      endIndex = message.length();
-    return message.substring(beginIndex, endIndex);
-  }
+        m_botAction.ipcSendMessage(getIPCChannel(), "info " + playerName + ":" + playerIP + ":" + playerMacID, getPubHubName(), "pubbotalias");
+    }
+
+    public void handleArenaMessage(String message) {
+        if (message.startsWith("IP:"))
+            sendPlayerInfo(message);
+    }
+
+    public void handleEvent(Message event) {
+        String message = event.getMessage();
+        int messageType = event.getMessageType();
+
+        if (messageType == Message.ARENA_MESSAGE)
+            handleArenaMessage(message);
+    }
+
+    public void handleEvent(PlayerEntered event) {
+        String playerName = event.getPlayerName();
+        if (playerName.startsWith("^") == false) {
+            m_botAction.sendUnfilteredPrivateMessage(playerName, "*info");
+        }
+    }
+    
+    public void handleEvent(FrequencyShipChange event) {
+        String name = m_botAction.getPlayerName(event.getPlayerID());
+        m_botAction.sendSmartPrivateMessage("WingZero", "fsc " + name);
+        if (!name.startsWith("^") && specs.contains(name.toLowerCase()))
+            m_botAction.sendUnfilteredPrivateMessage(name, "*info");
+    }
+
+    public void cancel() {
+    }
+
+    private String getInfo(String message, String infoName) {
+        int beginIndex = message.indexOf(infoName);
+        int endIndex;
+
+        if (beginIndex == -1)
+            return null;
+        beginIndex = beginIndex + infoName.length();
+        endIndex = message.indexOf("  ", beginIndex);
+        if (endIndex == -1)
+            endIndex = message.length();
+        return message.substring(beginIndex, endIndex);
+    }
 }
