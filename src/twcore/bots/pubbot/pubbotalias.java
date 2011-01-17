250b197529d610792a530dfe9e52f1a883828f73
WingZero <wingzerotw@gmail.com>
2011-01-17 13:46:41 +0000

    trying 1 more thing
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@4596 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubbot/pubbotalias.java
+++ src/twcore/bots/pubbot/pubbotalias.java
@@ -1,44 +1,18 @@
 package twcore.bots.pubbot;
 
-import java.util.HashSet;
-
 import twcore.bots.PubBotModule;
 import twcore.core.EventRequester;
-import twcore.core.events.FrequencyShipChange;
-import twcore.core.events.InterProcessEvent;
 import twcore.core.events.Message;
 import twcore.core.events.PlayerEntered;
-import twcore.core.util.ipc.IPCMessage;
 
 public class pubbotalias extends PubBotModule {
 
-    HashSet<String> specs = new HashSet<String>();
-    
     public void initializeModule() {
-        m_botAction.ipcSubscribe("banc");
     }
 
     public void requestEvents(EventRequester eventRequester) {
         eventRequester.request(EventRequester.MESSAGE);
         eventRequester.request(EventRequester.PLAYER_ENTERED);
-        eventRequester.request(EventRequester.FREQUENCY_SHIP_CHANGE);
-    }
-    
-    public void handleEvent(InterProcessEvent event) {
-        if (event.getChannel().equals("banc")) {
-            IPCMessage ipc = (IPCMessage)event.getObject();
-            String msg = ipc.getMessage();
-            String name;
-            if (msg.startsWith("addspec")) {
-                name = msg.substring(msg.indexOf(" ") + 1);
-                if (!specs.contains(name.toLowerCase()))
-                    specs.add(name.toLowerCase());
-            } else if (msg.startsWith("remspec")) {
-                name = msg.substring(msg.indexOf(" ") + 1);
-                if (specs.contains(name.toLowerCase()))
-                    specs.remove(name.toLowerCase());
-            }
-        }
     }
 
     public void sendPlayerInfo(String message) {
@@ -46,7 +20,7 @@ public class pubbotalias extends PubBotModule {
         String playerIP = getInfo(message, "IP:");
         String playerMacID = getInfo(message, "MachineId:");
 
-        m_botAction.ipcSendMessage(getIPCChannel(), "info " + playerName + ":" + playerIP + ":" + playerMacID, getPubHubName(), "pubbotalias");
+        m_botAction.ipcSendMessage(getIPCChannel(), "info " + playerName + ":" + playerIP + ":" + playerMacID, null, "pubbotalias");
     }
 
     public void handleArenaMessage(String message) {
@@ -61,30 +35,12 @@ public class pubbotalias extends PubBotModule {
         if (messageType == Message.ARENA_MESSAGE)
             handleArenaMessage(message);
     }
-    
-    public void sendSpecs() {
-        if (specs.isEmpty()) {
-            m_botAction.sendSmartPrivateMessage("WingZero", "Specs is empty");
-        } else {
-            String result = "Spec-locks: ";
-            for (String name : specs)
-                result += name + ", ";
-            result = result.substring(0, result.lastIndexOf(","));
-            m_botAction.sendSmartPrivateMessage("WingZero", result);
-        }
-    }
 
     public void handleEvent(PlayerEntered event) {
         String playerName = event.getPlayerName();
         if (playerName.startsWith("^") == false) {
             m_botAction.sendUnfilteredPrivateMessage(playerName, "*info");
         }
-    }
-    
-    public void handleEvent(FrequencyShipChange event) {
-        String name = m_botAction.getPlayerName(event.getPlayerID());
-        if (!name.startsWith("^") && specs.contains(name.toLowerCase()))
-            m_botAction.sendUnfilteredPrivateMessage(name, "*info");
     }
 
     public void cancel() {
