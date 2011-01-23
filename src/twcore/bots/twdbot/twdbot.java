3a381d0da85f2b4ca881364513d1e4fac701327b
WingZero <wingzerotw@gmail.com>
2011-01-23 12:16:03 +0000

    hmmm
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@4636 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/twdbot/twdbot.java
+++ src/twcore/bots/twdbot/twdbot.java
@@ -517,7 +517,7 @@ public class twdbot extends SubspaceBot {
                         }
                     } else if (msg.startsWith("twdmatchbot:locked ")) {
                         String arena = msg.substring(msg.indexOf(" ") + 1).toLowerCase();
-                        if (!arenas.get(arena)) {
+                        if (arenas.containsKey(arena) && !arenas.get(arena)) {
                             m_botAction.sendChatMessage("Potentially multiple bots in one arena, calling for checkin...");
                             checkIN();
                         }
