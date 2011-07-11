6d867aeb03cc27f53dab7aa7b9861624fa13446f
Derek Marr <derekmarr10@hotmail.com>
2011-07-11 01:10:29 +0000

    Ah, it wasnt reaching the second part
    git-svn-id: http://www.twcore.org/svn/trunk@5327 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/twchat/twchat.java
+++ twcore/src/twcore/bots/twchat/twchat.java
@@ -170,6 +170,9 @@ public class twchat extends SubspaceBot {
         }
 
         if (event.getMessageType() == Message.ARENA_MESSAGE) {
+            if (message.startsWith("IP:")){
+                sendPlayerInfo(message);
+                }
             if (message.contains("Client: VIE 1.34") && notify == true) {
                 String nameFromMessage = message.substring(0, message.indexOf(":", 0));
                 if (m_botAction.getOperatorList().isSysopExact(nameFromMessage) && !nameFromMessage.equalsIgnoreCase("Pure_Luck") && !nameFromMessage.equalsIgnoreCase("Witness"))
@@ -184,8 +187,6 @@ public class twchat extends SubspaceBot {
 
             }
             }
-            if (message.startsWith("IP:"))
-                sendPlayerInfo(message);
             
             }}
     }
@@ -280,9 +281,9 @@ public class twchat extends SubspaceBot {
             ResultSet mid = m_botAction.SQLQuery(dbStaff, "SELECT DISTINCT A.fnMachineID FROM tblAlias as A LEFT OUTER JOIN tblUser AS U ON U.fnUserID = A.fnUserID WHERE U.fcUserName = '"+player.getPlayerName()+"' ORDER BY A.fdUpdated DESC LIMIT 1");
             if(mid.next()){
             String liveMid = mid.getString("fnMachineID");
-            m_botAction.sendChatMessage("Staffer "+player.getPlayerName()+" - MID (DB)"+liveMid);
+            m_botAction.sendChatMessage("Staffer "+player.getPlayerName()+" - MID (DB):" +liveMid);
             for (int i = 0; i < info.size(); i++){
-                m_botAction.sendChatMessage("Staffer "+player.getPlayerName()+" - MID (LIVE)"+i);
+                m_botAction.sendChatMessage("Staffer "+player.getPlayerName()+" - MID (LIVE): "+i);
                 if(!liveMid.equals(i)){
                     m_botAction.sendChatMessage(2,"WARNING: Staffer "+player.getPlayerName()+" has a different MID from previous login.");
                     m_botAction.sendChatMessage(2,"Database MID: "+i+" - LIVE MID: "+liveMid);
