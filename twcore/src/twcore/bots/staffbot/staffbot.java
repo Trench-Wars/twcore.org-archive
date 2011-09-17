add0f66059a666131957b0698020e3a567c963da
WingZero <wingzerotw@gmail.com>
2011-09-17 08:16:14 +0000

    git-svn-id: http://www.twcore.org/svn/trunk@5843 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/staffbot/staffbot.java
+++ twcore/src/twcore/bots/staffbot/staffbot.java
@@ -42,17 +42,17 @@ import twcore.core.util.Tools;
 
 public class staffbot extends SubspaceBot {
 
-	private ModuleHandler moduleHandler;
+    private ModuleHandler moduleHandler;
 
-    OperatorList        m_opList;
-    BotAction           m_botAction;
-    BotSettings         m_botSettings;
-    TimerTask           getLog;
+    OperatorList m_opList;
+    BotAction m_botAction;
+    BotSettings m_botSettings;
+    TimerTask getLog;
     private final static int CHECK_LOG_DELAY = 2000;
 
     /* Initialization code */
-    public staffbot( BotAction botAction ) {
-        super( botAction );
+    public staffbot(BotAction botAction) {
+        super(botAction);
 
         moduleHandler = new ModuleHandler(botAction, botAction.getGeneralSettings().getString("Core Location") + "/twcore/bots/staffbot", "staffbot");
 
@@ -63,261 +63,261 @@ public class staffbot extends SubspaceBot {
         EventRequester req = botAction.getEventRequester();
         req.requestAll();
     }
-    
+
     @Override
     public void handleDisconnect() {
-    	moduleHandler.unloadAllModules();
+        moduleHandler.unloadAllModules();
     }
 
-	@Override
-	public void handleEvent(LoggedOn event) {
-		// join arena
-		m_botAction.joinArena(m_botSettings.getString("InitialArena"));
+    @Override
+    public void handleEvent(LoggedOn event) {
+        // join arena
+        m_botAction.joinArena(m_botSettings.getString("InitialArena"));
 
-		// join chats
-		// 1 = staff chat
-		// 2 = smod chat
-		String staffchat = m_botAction.getGeneralSettings().getString("Staff Chat");
-		String smodchat = m_botAction.getGeneralSettings().getString("Smod Chat");
-		m_botAction.sendUnfilteredPublicMessage("?chat="+staffchat+","+smodchat+",robodev");
+        // join chats
+        // 1 = staff chat
+        // 2 = smod chat
+        String staffchat = m_botAction.getGeneralSettings().getString("Staff Chat");
+        String smodchat = m_botAction.getGeneralSettings().getString("Smod Chat");
+        m_botAction.sendUnfilteredPublicMessage("?chat=" + staffchat + "," + smodchat + ",robodev");
 
-		// load modules
-		moduleHandler.loadModule("_serverwarningecho");
-		moduleHandler.loadModule("_warnings");
-		moduleHandler.loadModule("_badcommand_savelog");
-		moduleHandler.loadModule("_banc");
-		moduleHandler.loadModule("_staffchat_savelog");
-		//moduleHandler.loadModule("_commands");
-		
-		// start the log checking timer task for all modules
+        // load modules
+        moduleHandler.loadModule("_serverwarningecho");
+        moduleHandler.loadModule("_warnings");
+        moduleHandler.loadModule("_badcommand_savelog");
+        moduleHandler.loadModule("_banc");
+        moduleHandler.loadModule("_staffchat_savelog");
+        //moduleHandler.loadModule("_commands");
+
+        // start the log checking timer task for all modules
 
         // TimerTask to check the logs for *commands
         getLog = new TimerTask() {
             public void run() {
-                m_botAction.sendUnfilteredPublicMessage( "*log" );
+                m_botAction.sendUnfilteredPublicMessage("*log");
             }
         };
-        m_botAction.scheduleTaskAtFixedRate( getLog, 0, CHECK_LOG_DELAY );
-        
-	}
+        m_botAction.scheduleTaskAtFixedRate(getLog, 0, CHECK_LOG_DELAY);
 
-	@Override
-	public void handleEvent(Message event) {
-		if( ( event.getMessageType() == Message.PRIVATE_MESSAGE || event.getMessageType() == Message.REMOTE_PRIVATE_MESSAGE) &&
-				event.getMessage().startsWith("!")
-			 ) {
-			// Commands
-			String message = event.getMessage().toLowerCase();
-			short sender = event.getPlayerID();
-			String senderName = event.getMessageType() == Message.REMOTE_PRIVATE_MESSAGE ? event.getMessager() :  m_botAction.getPlayerName(sender);
-			int operatorLevel = m_botAction.getOperatorList().getAccessLevel(senderName);;
+    }
 
-			// Ignore player's commands
-			if(operatorLevel == OperatorList.PLAYER_LEVEL) {
-				return;
-			}
+    @Override
+    public void handleEvent(Message event) {
+        if ((event.getMessageType() == Message.PRIVATE_MESSAGE || event.getMessageType() == Message.REMOTE_PRIVATE_MESSAGE)
+                && event.getMessage().startsWith("!")) {
+            // Commands
+            String message = event.getMessage().toLowerCase();
+            short sender = event.getPlayerID();
+            String senderName = event.getMessageType() == Message.REMOTE_PRIVATE_MESSAGE ? event.getMessager() : m_botAction.getPlayerName(sender);
+            int operatorLevel = m_botAction.getOperatorList().getAccessLevel(senderName);
+            ;
 
-			// !help
-			if(message.startsWith("!help")) {
-				String[] help = {
-						" Op: "+senderName+" ("+Tools.staffName(operatorLevel)+")",
-						"-----------------------[ Staff Bot ]-----------------------",
-                        " !isStaff <name>           - Checks if <name> is a member of staff"
-				};
+            // Ignore player's commands
+            if (operatorLevel == OperatorList.PLAYER_LEVEL) {
+                return;
+            }
 
-				String[] smodHelp = {
-						" !die                      - Disconnects Staffbot"
-				};
-				
-				String[] ownerHelp = {
-				        " !putfile                  - Uploads file to the zone (RARE CASE)"
-				};
+            // !help
+            if (message.startsWith("!help")) {
+                String[] help = { " Op: " + senderName + " (" + Tools.staffName(operatorLevel) + ")",
+                        "-----------------------[ Staff Bot ]-----------------------",
+                        " !isStaff <name>           - Checks if <name> is a member of staff" };
 
-				m_botAction.smartPrivateMessageSpam(senderName, help);
+                String[] smodHelp = { " !die                      - Disconnects Staffbot" };
 
-				if(m_botAction.getOperatorList().isSmod(senderName)) {
-					m_botAction.smartPrivateMessageSpam(senderName, smodHelp);
-				}
-				if(m_botAction.getOperatorList().isOwner(senderName)){
-				    m_botAction.smartPrivateMessageSpam(senderName, ownerHelp);
-				}
-			}
-			if(message.equalsIgnoreCase("!die")) {
-				moduleHandler.unloadAllModules();
-				this.handleDisconnect();
-				m_botAction.die();
-			}
-			if(m_botAction.getOperatorList().isOwner(senderName)){
-			if(message.startsWith("!putfile ")){
-			    String msg = message.substring(9);
-			    m_botAction.putFile(msg);
-			    m_botAction.sendSmartPrivateMessage(senderName, "Done.");
-		
-		} else if(message.startsWith("!getfile ")){
-			    String msg = message.substring(9);
-			    m_botAction.getServerFile(msg);
-	            m_botAction.sendSmartPrivateMessage(senderName, "Done.");
-			}}
+                String[] ownerHelp = { " !putfile                  - Uploads file to the zone (RARE CASE)" };
 
-            if( message.toLowerCase().startsWith("!isstaff") ) {
+                m_botAction.smartPrivateMessageSpam(senderName, help);
+
+                if (m_botAction.getOperatorList().isSmod(senderName)) {
+                    m_botAction.smartPrivateMessageSpam(senderName, smodHelp);
+                }
+                if (m_botAction.getOperatorList().isOwner(senderName)) {
+                    m_botAction.smartPrivateMessageSpam(senderName, ownerHelp);
+                }
+            }
+            if (message.equalsIgnoreCase("!die")) {
+                moduleHandler.unloadAllModules();
+                this.handleDisconnect();
+                m_botAction.die();
+            }
+            if (m_botAction.getOperatorList().isOwner(senderName)) {
+                if (message.startsWith("!putfile ")) {
+                    String msg = message.substring(9);
+                    m_botAction.putFile(msg);
+                    m_botAction.sendSmartPrivateMessage(senderName, "Done.");
+                } else if (message.startsWith("!getfile ")) {
+                    String msg = message.substring(9);
+                    m_botAction.getServerFile(msg);
+                    m_botAction.sendSmartPrivateMessage(senderName, "Done.");
+                } else if (message.startsWith("!echo ")) {
+                    String msg = message.substring(message.indexOf(" ") + 1);
+                    m_botAction.sendUnfilteredPublicMessage(msg);
+                    m_botAction.sendSmartPrivateMessage(senderName, "Echoed: " + msg);
+                }
+            }
+
+            if (message.toLowerCase().startsWith("!isstaff")) {
                 String[] parse = message.split(" ", 2);
-                if( parse.length == 2 ) {
-                    int accessLevel = m_botAction.getOperatorList().getAccessLevel( parse[1] );
-                    if( accessLevel == 0 ) {
-                        m_botAction.sendSmartPrivateMessage( senderName, "'" + parse[1] + "' is not a member of staff, or the name was not found (use exact case, i.e., 'DoCk>')." );
+                if (parse.length == 2) {
+                    int accessLevel = m_botAction.getOperatorList().getAccessLevel(parse[1]);
+                    if (accessLevel == 0) {
+                        m_botAction.sendSmartPrivateMessage(senderName, "'" + parse[1]
+                                + "' is not a member of staff, or the name was not found (use exact case, i.e., 'DoCk>').");
                     } else {
-                        if( m_botAction.getOperatorList().isHighmod(senderName) ) {
-                            m_botAction.sendSmartPrivateMessage( senderName, "'" + parse[1] + "' is staff: " + m_botAction.getOperatorList().getAccessLevelName(accessLevel) );
+                        if (m_botAction.getOperatorList().isHighmod(senderName)) {
+                            m_botAction.sendSmartPrivateMessage(senderName, "'" + parse[1] + "' is staff: "
+                                    + m_botAction.getOperatorList().getAccessLevelName(accessLevel));
                         } else {
-                            m_botAction.sendSmartPrivateMessage( senderName, "'" + parse[1] + "' is a member of staff." );
+                            m_botAction.sendSmartPrivateMessage(senderName, "'" + parse[1] + "' is a member of staff.");
                         }
 
                     }
                 }
             }
-		}
+        }
 
-		moduleHandler.handleEvent(event);
-	}
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(SubspaceEvent event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(SubspaceEvent event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(ArenaJoined event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(ArenaJoined event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(ArenaList event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(ArenaList event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(BallPosition event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(BallPosition event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FileArrived event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FileArrived event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FlagClaimed event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FlagClaimed event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FlagDropped event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FlagDropped event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FlagPosition event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FlagPosition event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FlagReward event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FlagReward event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FlagVictory event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FlagVictory event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FrequencyChange event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FrequencyChange event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(FrequencyShipChange event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(FrequencyShipChange event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(InterProcessEvent event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(InterProcessEvent event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(KotHReset event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(KotHReset event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(PlayerBanner event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(PlayerBanner event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(PlayerDeath event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(PlayerDeath event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(PlayerEntered event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(PlayerEntered event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(PlayerLeft event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(PlayerLeft event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(PlayerPosition event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(PlayerPosition event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(Prize event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(Prize event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(ScoreReset event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(ScoreReset event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(ScoreUpdate event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(ScoreUpdate event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(SoccerGoal event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(SoccerGoal event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(SQLResultEvent event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(SQLResultEvent event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(TurfFlagUpdate event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(TurfFlagUpdate event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(TurretEvent event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(TurretEvent event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(WatchDamage event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(WatchDamage event) {
+        moduleHandler.handleEvent(event);
+    }
 
-	@Override
-	public void handleEvent(WeaponFired event) {
-		moduleHandler.handleEvent(event);
-	}
+    @Override
+    public void handleEvent(WeaponFired event) {
+        moduleHandler.handleEvent(event);
+    }
 
 }
