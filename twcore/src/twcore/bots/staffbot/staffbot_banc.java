3ac3085aaefb320f69ad03ae06bed414d6453710
WingZero <wingzerotw@gmail.com>
2012-03-28 23:08:21 +0000

    git-svn-id: http://www.twcore.org/svn/trunk@6394 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/staffbot/staffbot_banc.java
+++ twcore/src/twcore/bots/staffbot/staffbot_banc.java
@@ -1343,7 +1343,7 @@ public class staffbot_banc extends Module {
         m_botAction.sendRemotePrivateMessage(name, "Please do not forget to add comments to your BanC with !bancomment <#id> <comments>.");
         m_botAction.ipcSendMessage(IPCBANC, bancType.toString() + " " + time + ":" + target, null, "banc");
         if (bancType == BanCType.SILENCE)
-            m_botAction.ipcTransmit(IPCBANC, new IPCEvent(banc, 0, -1));
+            m_botAction.ipcTransmit(IPCBANC, new IPCEvent(banc.getPlayername() + ":" + (banc.getIP() != null ? banc.getIP() : " ") + ":" + (banc.getMID() != null ? banc.getMID() : " ") + ":" + banc.getDuration() + ":" + banc.getType().toString(), 0, -1));
 
         if (comment != null)
             cmdBancomment(name, "#" + banc.getId() + " " + comment);
