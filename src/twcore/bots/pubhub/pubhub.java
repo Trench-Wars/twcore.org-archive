f3fafde0c93604be044305b5e25fd0a08a748778
WingZero <wingzerotw@gmail.com>
2012-03-29 06:58:42 +0000

    been meaning to fix that minor typo
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@6399 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubhub/pubhub.java
+++ src/twcore/bots/pubhub/pubhub.java
@@ -554,7 +554,7 @@ public class pubhub extends SubspaceBot {
         for(String bot:pubbots.keySet()) {
             if(bot.startsWith("SPAWNING")) {
                 arena = pubbots.get(bot);
-                if (Tools.isAllDigits(arena) || arena.contains("tw") || arena.contains("trenchwars"))
+                if (Tools.isAllDigits(arena) || arena.equalsIgnoreCase("tw") || arena.equalsIgnoreCase("trenchwars"))
                     break;
             }
         }
