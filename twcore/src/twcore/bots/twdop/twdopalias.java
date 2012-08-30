8ea8c9de0f1e54e019bac8a298ca210f63964fe7
WingZero <wingzerotw@gmail.com>
2012-08-30 15:45:31 +0000

    testing info
    git-svn-id: http://www.twcore.org/svn/trunk@6625 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/twdop/twdopalias.java
+++ twcore/src/twcore/bots/twdop/twdopalias.java
@@ -227,6 +227,19 @@ public class twdopalias extends Module {
         }
     }
 
+    private void doInfoAllCmd(String playerName) {
+        try {
+            String[] headers = { IP_FIELD, MID_FIELD, TIMES_UPDATED_FIELD, LAST_UPDATED_FIELD };
+            displayAltNickAllResults(
+                    "SELECT * "
+                            + "FROM `tblAlias` INNER JOIN `tblUser` ON `tblAlias`.fnUserID = `tblUser`.fnUserID "
+                            + "WHERE fcUserName = '" + Tools.addSlashesToString(playerName) + "' "
+                            + getOrderBy(), headers, null);
+        } catch (SQLException e) {
+            throw new RuntimeException("SQL Error: " + e.getMessage(), e);
+        }
+    }
+
     private long make32BitIp(String ipString) {
         StringTokenizer stringTokens = new StringTokenizer(ipString, ".");
         String ipPart;
@@ -777,6 +790,8 @@ public class twdopalias extends Module {
             //              doAltTWLCmd(message.substring(8).trim());
             else if (command.startsWith("!info "))
                 doInfoCmd(message.substring(6).trim());
+            else if (opList.isSysopExact(sender) && command.startsWith("!infoall "))
+                doInfoAllCmd(message.substring(9).trim());
             else if (command.startsWith("!compare "))
                 doCompareCmd(message.substring(9).trim());
             else if (command.startsWith("!maxrecords "))
