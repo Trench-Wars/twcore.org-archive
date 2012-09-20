2dad7e0794cfdd7283520b40b1045bcc07515a17
WingZero <wingzerotw@gmail.com>
2012-09-20 07:56:49 +0000

    added warping feature added 5 second delay and arena msg prior to map change
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@6708 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubsystem/module/PubMapModule.java
+++ src/twcore/bots/pubsystem/module/PubMapModule.java
@@ -1,6 +1,11 @@
 package twcore.bots.pubsystem.module;
 
+import java.awt.Point;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.TimerTask;
 
 import twcore.bots.pubsystem.PubContext;
 import twcore.core.BotAction;
@@ -9,6 +14,8 @@ import twcore.core.EventRequester;
 import twcore.core.events.FrequencyShipChange;
 import twcore.core.events.PlayerEntered;
 import twcore.core.events.PlayerLeft;
+import twcore.core.game.Player;
+import twcore.core.util.MapRegions;
 import twcore.core.util.Tools;
 
 public class PubMapModule extends AbstractModule {
@@ -20,6 +27,13 @@ public class PubMapModule extends AbstractModule {
     private static final int SMALL_BASE = 6;
     private static final int MED_BASE = 9;
     private static final int LARGE_BASE = 8;
+    private static final String MAP_NAME = "pub";
+
+    private static final int LARGE_REGION = 2;
+    private static final int MED_REGION = 3;
+    private static final int SMALL_REGION = 4;
+    private static final int FR_REGION = 1;
+    private static final int MID_REGION = 0;
     
     private int currentBase;    // current door setting
 
@@ -30,13 +44,16 @@ public class PubMapModule extends AbstractModule {
     private long lastChange;   
     
     private BotAction ba;
+    private Random random;
+    private MapRegions regions;
     
     public PubMapModule(BotAction botAction, PubContext context) {
         super(botAction, context, "PubMap");
         ba = botAction;
+        random = new Random();
+        regions = new MapRegions();
         lastChange = 0;
         currentBase = MED_BASE;
-        setBase();
         reloadConfig();
     }
 
@@ -67,7 +84,23 @@ public class PubMapModule extends AbstractModule {
         popLeeway = set.getInt("PopulationLeeway");
         timeDelay = set.getInt("TimeDelay");
         enabled = set.getInt("pubmap_enabled") == 1;
+        reloadRegions();
         doPopCheck();
+    }
+    
+    public void reloadRegions() {
+        try {
+            regions.clearRegions();
+            regions.loadRegionImage(MAP_NAME + ".png");
+            regions.loadRegionCfg(MAP_NAME + ".cfg");
+        } catch (FileNotFoundException fnf) {
+            Tools.printLog("Error: " + MAP_NAME + ".png and " + MAP_NAME + ".cfg must be in the data/maps folder.");
+        } catch (javax.imageio.IIOException iie) {
+            Tools.printLog("Error: couldn't read image");
+        } catch (Exception e) {
+            Tools.printLog("Could not load warps for " + MAP_NAME);
+            Tools.printStackTrace(e);
+        }
     }
     
     public void handleEvent(PlayerEntered event) {
@@ -109,43 +142,57 @@ public class PubMapModule extends AbstractModule {
         long now = System.currentTimeMillis();
         if (lastChange != 0 && now - lastChange < timeDelay * Tools.TimeInMillis.MINUTE)
             return;
-        if (pop > popTriggers.get(LARGE_BASE) + popLeeway) {
-            lastChange = now;
-            currentBase = LARGE_BASE;
-            setBase();
-        } else if (pop < popTriggers.get(SMALL_BASE) - popLeeway) {
-            lastChange = now;
-            currentBase = SMALL_BASE;
-            setBase();
-        } else if (currentBase == SMALL_BASE && pop > popTriggers.get(SMALL_BASE) + popLeeway) {
-            lastChange = now;
-            currentBase = MED_BASE;
-            setBase();
-        } else if (currentBase == LARGE_BASE && pop < popTriggers.get(LARGE_BASE) - (popLeeway*2)) {
-            lastChange = now;
-            currentBase = MED_BASE;
-            setBase();
+        if (pop > popTriggers.get(LARGE_BASE) + popLeeway)
+            setBase(LARGE_BASE);
+        else if (pop < popTriggers.get(SMALL_BASE) - popLeeway)
+            setBase(SMALL_BASE);
+        else if (currentBase == SMALL_BASE && pop > popTriggers.get(SMALL_BASE) + popLeeway)
+            setBase(MED_BASE);
+        else if (currentBase == LARGE_BASE && pop < popTriggers.get(LARGE_BASE) - (popLeeway*2))
+            setBase(MED_BASE);
+    }
+    
+    private String getBase(int base) {
+        if (base == SMALL_BASE)
+            return "SMALL";
+        else if (base == MED_BASE)
+            return "MEDIUM";
+        else
+            return "LARGE";
+    }
+    
+    private void setBase(int base) {
+        if (currentBase == base)
+            return;
+        ba.sendArenaMessage("NOTICE: The map will be changing to " + getBase(base) + " in 5 seconds!", 24);
+        ba.scheduleTask(new BaseChange(base), 5000);
+    }
+    
+    private void warpForMedium() {
+        Iterator<Player> i = ba.getPlayingPlayerIterator();
+        while (i.hasNext()) {
+            Player p = i.next();
+            int reg = regions.getRegion(p);
+            if (reg == LARGE_REGION) {
+                Point coord = getRandomPoint(FR_REGION);
+                ba.warpTo(p.getPlayerID(), (int) coord.getX(), (int) coord.getY());
+            }
         }
     }
     
-    private void setBase() {
-        ba.setDoors(currentBase);
-        switch (currentBase) {
-            case SMALL_BASE:
-                ba.showObject(SMALL_OBJON);
-                ba.hideObject(MED_OBJON);
-                ba.hideObject(LARGE_OBJON);
-                break;
-            case MED_BASE:
-                ba.showObject(MED_OBJON);
-                ba.hideObject(SMALL_OBJON);
-                ba.hideObject(LARGE_OBJON);
-                break;
-            case LARGE_BASE:
-                ba.showObject(LARGE_OBJON);
-                ba.hideObject(SMALL_OBJON);
-                ba.hideObject(MED_OBJON);
-                break;
+    private void warpForSmall() {
+        Iterator<Player> i = ba.getPlayingPlayerIterator();
+        while (i.hasNext()) {
+            Player p = i.next();
+            int reg = regions.getRegion(p);
+            if (reg == LARGE_REGION || reg == MED_REGION) {
+                Point coord = getRandomPoint(FR_REGION);
+                ba.sendPublicMessage("Warped " + p.getPlayerName() + ": " + coord.getX() + " " + coord.getY());
+                ba.warpTo(p.getPlayerID(), (int) coord.getX(), (int) coord.getY());
+            } else if (reg == SMALL_REGION) {
+                Point coord = getRandomPoint(MID_REGION);
+                ba.warpTo(p.getPlayerID(), (int) coord.getX(), (int) coord.getY());
+            }
         }
     }
 
@@ -166,7 +213,7 @@ public class PubMapModule extends AbstractModule {
         else if (command.equals("!mapmod"))
             cmd_mapMod(sender);
         else if (command.equals("!setbase"))
-            setBase();
+            setBase(currentBase);
     }
     
     private void cmd_reloadConfig(String name) {
@@ -216,6 +263,66 @@ public class PubMapModule extends AbstractModule {
                 " - !mapmod           -- Enables/Disables the pub map population control.",
         };
         return msg;
+    }
+
+    /**
+     * Generates a random point within the specified region
+     * 
+     * @param region
+     *            the region the point should be in
+     * @return A point object, or null if the first 10000 points generated were not within the desired region.
+     */
+    private Point getRandomPoint(int region) {
+        Point p = null;
+        int count = 0;
+        while (p == null) {
+            int x = random.nextInt(590-430) + 430;
+            int y = random.nextInt(340-250) + 250;
+
+            p = new Point(x, y);
+            if (!regions.checkRegion(x, y, region))
+                p = null;
+            count++;
+            if (count > 100000) {
+                if (random.nextBoolean())
+                    p = new Point(478, 308);
+                else
+                    p = new Point(545, 308);
+            }
+        }
+        return p;
+    }
+    
+    private class BaseChange extends TimerTask {
+        private int base;
+        
+        public BaseChange(int base) {
+            this.base = base;
+        }
+        
+        public void run() {
+            currentBase = base;
+            ba.setDoors(currentBase);
+            switch (currentBase) {
+                case SMALL_BASE:
+                    ba.showObject(SMALL_OBJON);
+                    ba.hideObject(MED_OBJON);
+                    ba.hideObject(LARGE_OBJON);
+                    warpForSmall();
+                    break;
+                case MED_BASE:
+                    ba.showObject(MED_OBJON);
+                    ba.hideObject(SMALL_OBJON);
+                    ba.hideObject(LARGE_OBJON);
+                    warpForMedium();
+                    break;
+                case LARGE_BASE:
+                    ba.showObject(LARGE_OBJON);
+                    ba.hideObject(SMALL_OBJON);
+                    ba.hideObject(MED_OBJON);
+                    break;
+            }
+        }
     }
 
 }
