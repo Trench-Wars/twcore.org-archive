db300456860d0f16335e3471cd61012f598ea8f1
WingZero <wingzerotw@gmail.com>
2012-09-28 00:12:06 +0000

    moved kick messages to smod chat
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@6745 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/staffbot/staffbot_banc.java
+++ src/twcore/bots/staffbot/staffbot_banc.java
@@ -375,7 +375,7 @@ public class staffbot_banc extends Module {
             if (command.equals("BANC PUBBOT INIT"))
                 sendIPCActiveBanCs(ipc.getSender());
             else if (command.startsWith("KICKED:"))
-                m_botAction.sendChatMessage(command.substring(7));
+                m_botAction.sendChatMessage(2, command.substring(7));
             else if (command.startsWith(BanCType.SILENCE.toString())) {
                 BanC banc = lookupActiveBanC(BanCType.SILENCE, args[1]);
                 if (banc != null && banc.isNotification())
