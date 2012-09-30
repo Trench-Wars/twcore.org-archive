a585ff402248e0d73aef10341ddf630eb587aed1
SpookedOne <spookedone@gmail.com>
2012-09-30 04:35:40 +0000

    http://twcore.org/ticket/843
    git-svn-id: http://www.twcore.org/svn/trunk@6747 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- tools/TrenchStreamBot/src/trenchstreambot/TrenchStreamBot.java
+++ tools/TrenchStreamBot/src/trenchstreambot/TrenchStreamBot.java
@@ -6,15 +6,23 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 /**
- *
+ * TrenchStreamBot
+ * 
+ * Simple bot to assist our stream for spectating players in pub. Launches 
+ * continuum and pages to bottom of player list. Cycles through a set amount of 
+ * player by jumping up two players on the list and returning once. 
+ * 
  * @author SpookedOne
  */
 public class TrenchStreamBot {
 
     private static final String EXEC_PATH = 
-            "c:/program files(x86)/continuum/continuum.exe";
+            "C:/Program Files (x86)/Continuum/continuum.exe";
     private static Robot bot;
 
+    private static final int SPEC_DELAY = 12; //in seconds
+    private static final int SPEC_CYCLE = 10; //num of ppl before reset
+    
     /**
      * @param args the command line arguments
      */
@@ -34,7 +42,9 @@ public class TrenchStreamBot {
         bot.delay(10 * 1000); //wait 10s for continuum to load
         type(KeyEvent.VK_ENTER); //press enter to join
         
-        bot.delay(5 * 60 * 1000); //wait 5m for load (new map dl)
+        bot.delay(30 * 1000); //wait 30s for load (not handling new map dl)
+        
+        type(KeyEvent.VK_F2); //keep player list open
         
         //press pg down 40*
         for(int i = 0; i < 40; i++) {
@@ -42,32 +52,34 @@ public class TrenchStreamBot {
         }
         
         long counter = 0;
-        long pager = 0;
+        boolean page = false;
         do {
 
-            //every 30s, change spec
-            if (counter % 30 == 0) {
-                if (pager == 8) {
-                    for (int i = 0; i < 8; i++) {
+            //change spec
+            if (counter % SPEC_DELAY == 0) {
+                
+                //reset after cycle complete
+                if (counter % ((SPEC_CYCLE - 1) * 2) == 0) {
+                    //press pg down 40*
+                    for(int i = 0; i < 40; i++) {
                         type(KeyEvent.VK_PAGE_DOWN);
                     }
-                    pager = 0;
+                    page = false;
+                }
+                
+                if (page) {
+                    for (int i = 0; i < 1; i++) {
+                        type(KeyEvent.VK_PAGE_DOWN);
+                    }
+                    page = false;
                 } else {
-                    for (int i = 0; i < 8; i++) {
+                    for (int i = 0; i < 2; i++) {
                         type(KeyEvent.VK_PAGE_UP);
                     }
-                    pager++;
+                    page = true;
                 }
-                type(KeyEvent.VK_CONTROL);
-            }
-            
-            //at 5m, keep alive/reset
-            if (counter == 5 * 60) {
-                type(":TrenchStream:Play TrenchWars!");
-                type(KeyEvent.VK_ENTER);
-                
-                //reset counter so not to get too high
-                counter = 0;
+                bot.delay(200); //wait 200ms
+                type(KeyEvent.VK_CONTROL, 40); //delay set due to slow response
             }
 
             bot.delay(1 * 1000); //sleep for 1s
@@ -82,6 +94,13 @@ public class TrenchStreamBot {
         bot.keyRelease(i);
     }
 
+    private static void type(int i, int delay) {
+        bot.delay(40);
+        bot.keyPress(i);
+        bot.delay(delay);
+        bot.keyRelease(i);
+    }
+    
     private static void type(String s) {
         byte[] bytes = s.getBytes();
         for (byte b : bytes) {
