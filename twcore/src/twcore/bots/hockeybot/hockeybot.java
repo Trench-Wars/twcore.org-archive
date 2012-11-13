9f22e3736f2eaaabbd6951367d6b9bc7ab1b943c
WingZero <wingzerotw@gmail.com>
2012-11-13 15:46:20 +0000

    git-svn-id: http://www.twcore.org/svn/trunk@6988 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/hockeybot/hockeybot.java
+++ twcore/src/twcore/bots/hockeybot/hockeybot.java
@@ -114,10 +114,6 @@ public class hockeybot extends SubspaceBot {
         lockLastGame = false;
 
         botCrease = new Stack<String>();
-        m_botAction.setMessageLimit( 8, false );
-        m_botAction.setReliableKills( 1 );
-        m_botAction.setPlayerPositionUpdating( 675 );
-        m_botAction.setLowPriorityPacketCap( 8 );
 
     }
 
@@ -1747,6 +1743,10 @@ public class hockeybot extends SubspaceBot {
      * Starts the bot
      */
     private void start() {
+        m_botAction.setMessageLimit( 8, false );
+        m_botAction.setReliableKills( 1 );
+        m_botAction.setPlayerPositionUpdating( 675 );
+        m_botAction.setLowPriorityPacketCap( 8 );
         lockLastGame = false;
         lockArena();
         lockDoors();
