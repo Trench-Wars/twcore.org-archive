8601695486dd8e6c4368ef1755ca91fa470703d0
WingZero <wingzerotw@gmail.com>
2013-05-12 23:23:46 +0000

    git-svn-id: http://www.twcore.org/svn/trunk/twcore@7295 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubbot/pubbotbanc.java
+++ src/twcore/bots/pubbot/pubbotbanc.java
@@ -187,6 +187,7 @@ public class pubbotbanc extends PubBotModule {
                 m_botAction.sendUnfilteredPrivateMessage(name, "*kill");
                 m_botAction.ipcSendMessage(IPCBANC, "KICKED:Player '" + name + "' has been kicked by " + m_botAction.getBotName() + " for using an unapproved proxy. (" + proxy + ")", "banc", m_botAction.getBotName());
             } else if (message.contains("Proxy: ") && !message.contains("Not using proxy")) {
+                String proxy = message.substring(message.indexOf("Proxy:"), message.indexOf("Idle:")-1);
                 String name = message.substring(0, message.indexOf(": U"));
                 m_botAction.ipcSendMessage(IPCBANC, "PROXY:Player '" + name + "' detected by " + m_botAction.getBotName() + " using a proxy. (" + proxy + ")", "banc", m_botAction.getBotName());
             } else if (message.startsWith("IP:"))
