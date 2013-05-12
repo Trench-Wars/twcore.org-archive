d1a0c165a3bc3c4a023fc094904f8d05e20f4d91
WingZero <wingzerotw@gmail.com>
2013-05-12 23:20:04 +0000

    git-svn-id: http://www.twcore.org/svn/trunk@7293 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/staffbot/staffbot_banc.java
+++ twcore/src/twcore/bots/staffbot/staffbot_banc.java
@@ -337,7 +337,7 @@ public class staffbot_banc extends Module {
                 sendIPCActiveBanCs(ipc.getSender());
             else if (command.startsWith("ELAPSED:"))
                 doElapsed(args[1], args[2]);
-            else if (command.startsWith("KICKED:"))
+            else if (command.startsWith("KICKED:") || command.startsWith("PROXY:"))
                 m_botAction.sendChatMessage(2, command.substring(7));
             else if (command.startsWith(BanCType.SILENCE.toString())) {
                 BanC banc = lookupActiveBanC(BanCType.SILENCE, args[1]);
