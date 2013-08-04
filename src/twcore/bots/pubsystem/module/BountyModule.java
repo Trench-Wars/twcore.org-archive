11b357babe00bf3644de60d51517b7ec34c247da
WingZero <wingzerotw@gmail.com>
2013-08-04 04:50:28 +0000

    revert
    git-svn-id: http://www.twcore.org/svn/trunk/twcore@7529 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- src/twcore/bots/pubsystem/module/BountyModule.java
+++ src/twcore/bots/pubsystem/module/BountyModule.java
@@ -4,9 +4,8 @@
  */
 package twcore.bots.pubsystem.module;
 
-import java.util.ConcurrentModificationException;
 import java.util.List;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import twcore.bots.pubsystem.PubContext;
 import twcore.bots.pubsystem.module.player.PubPlayer;
 import twcore.bots.pubsystem.pubsystem;
@@ -17,27 +16,29 @@ import twcore.core.game.Player;
 
 /**
  * PubSystem Bounty Module
- *
+ * 
  * Allows players to place bounties on each other in pub.
- *
+ * 
  * @see http://www.twcore.org/ticket/874
  * @author SpookedOne
  */
 public class BountyModule extends AbstractModule {
 
-    private HashMap<String, Integer> bounties;
+    private ConcurrentHashMap<String, Integer> bounties;
+    
     private int minimumBounty;
     private int maximumBounty;
+    
     private boolean isRunning;
-
+    
     public BountyModule(BotAction botAction, PubContext context) {
         super(botAction, context, "Bounty");
-
-        bounties = new HashMap<String, Integer>();
-
+        
+        bounties = new ConcurrentHashMap<String, Integer>();
+        
         reloadConfig();
     }
-
+    
     @Override
     public void start() {
         reloadConfig();
@@ -58,25 +59,23 @@ public class BountyModule extends AbstractModule {
     @Override
     public void handleEvent(PlayerDeath event) {
         if (isRunning) {
-            try {
-                Player killed = m_botAction.getPlayer(event.getKilleeID());
-                Player killer = m_botAction.getPlayer(event.getKillerID());
-                if (killed != null && killer != null) {
-                    Integer amount = bounties.get(killed);
-                    if (amount != null) {
-                        PubPlayer killerPubPlayer = context.getPlayerManager()
-                                .getPlayer(killer.getPlayerName());
-                        killerPubPlayer.addMoney(amount);
-                        m_botAction.sendPrivateMessage(killer.getPlayerName(),
-                                "[Bounty] You have been awarded $" + amount
-                                + " for the death of " + killed.getPlayerName());
-                    }
+            Player killed = m_botAction.getPlayer(event.getKilleeID());
+            Player killer = m_botAction.getPlayer(event.getKillerID());
+            if (killed != null && killer != null) {
+                Integer amount = bounties.get(killed);
+                if (amount != null) {
+                    PubPlayer killerPubPlayer = context.getPlayerManager()
+                            .getPlayer(killer.getPlayerName());
+                    killerPubPlayer.addMoney(amount);
+                    m_botAction.sendPrivateMessage(killer.getPlayerName(), 
+                            "[Bounty] You have been awarded $" + amount + 
+                            " for the death of " + killed.getPlayerName());
                 }
-            } catch (ConcurrentModificationException e) {
             }
         }
     }
-
+    
+    
     @Override
     public void reloadConfig() {
         minimumBounty = m_botAction.getBotSettings().getInt("bounty_min");
@@ -97,8 +96,8 @@ public class BountyModule extends AbstractModule {
     public void handleModCommand(String sender, String command) {
         String in = command.toLowerCase();
         if (in.startsWith("!status")) {
-            m_botAction.sendPrivateMessage(sender, "[Bounty] Module is "
-                    + (isRunning ? "" : "not") + " running.");
+            m_botAction.sendPrivateMessage(sender, "[Bounty] Module is " + 
+                    (isRunning?"":"not") + " running.");
         }
     }
 
@@ -115,71 +114,74 @@ public class BountyModule extends AbstractModule {
 
     @Override
     public String[] getHelpMessage(String sender) {
-        return new String[]{
+        return new String[] {
             pubsystem.getHelpLine("!listbty             -- List current bounties."),
             pubsystem.getHelpLine("!listbty <name>      -- List current bounty on <name>."),
-            pubsystem.getHelpLine("!addbty <name>:<amt> -- Add <amt> of bounty to player <name>."),};
+            pubsystem.getHelpLine("!addbty <name>:<amt> -- Add <amt> of bounty to player <name>."),
+        };
     }
 
     @Override
     public String[] getModHelpMessage(String sender) {
-        return new String[]{
-            pubsystem.getHelpLine("!status              -- Shows status of the Bounty module."),};
+        return new String[] {
+            pubsystem.getHelpLine("!status              -- Shows status of the Bounty module."),
+        };
     }
 
     @Override
     public String[] getSmodHelpMessage(String sender) {
-        return new String[]{
-            pubsystem.getHelpLine("!reloadbty           -- Reload the configuration file. (Restarts)"),};
+        return new String[] {
+            pubsystem.getHelpLine("!reloadbty           -- Reload the configuration file. (Restarts)"),
+        };
     }
-
+    
     private void listBounty(String sender, String command) {
         String name = null;
         if (command.length() > "!listbty ".length()) {
             name = command.substring("!listbty ".length());
         }
-
+        
         if (name != null) {
             Player lookup = m_botAction.getFuzzyPlayer(name);
             if (lookup != null) {
                 Integer bounty = bounties.get(lookup.getPlayerName());
                 m_botAction.sendPrivateMessage(sender, "[Bounty] " + lookup.getPlayerName()
-                        + " has a $" + (bounty == null ? "0" : bounty) + " bounty.");
+                        + " has a $" + (bounty==null?"0":bounty) + " bounty.");
             }
         } else {
             List<Player> players = m_botAction.getPlayingPlayers();
             for (Player p : players) {
                 Integer bounty = bounties.get(p.getPlayerName());
                 m_botAction.sendPrivateMessage(sender, "[Bounty] " + p.getPlayerName()
-                        + " has a $" + (bounty == null ? "0" : bounty) + " bounty.");
+                        + " has a $" + (bounty==null?"0":bounty) + " bounty.");
             }
         }
     }
-
+    
     private void addBounty(String sender, String command) {
         PubPlayer requester = context.getPlayerManager().getPlayer(sender);
         String[] parameters = null;
         Player deadman = null;
-
+        
         if (command.length() > "!addbty ".length()) {
             String sub = command.substring("!addbty ".length());
             parameters = sub.split(":");
         }
-
+        
         if (parameters != null) {
             deadman = m_botAction.getFuzzyPlayer(parameters[0]);
         }
-
+        
         if (requester != null && deadman != null) {
-
+            
             Integer currentAmount = bounties.get(deadman.getPlayerName());
             if (currentAmount == null) {
                 currentAmount = 0;
             }
-
+            
             try {
                 Integer addition = Integer.parseInt(parameters[1]);
-
+                
                 if (addition < minimumBounty || addition > maximumBounty) {
                     throw new NumberFormatException();
                 } else if (addition + currentAmount > maximumBounty) {
