2f683e330fef98fe971ecb52d46cbda00b7d5aa1
ThePAP <p_neger@hotmail.com>
2014-07-13 16:30:36 +0000

    Additional debug
    git-svn-id: http://www.twcore.org/svn/trunk@8843 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/matchbot/matchbot.java
+++ twcore/src/twcore/bots/matchbot/matchbot.java
@@ -551,7 +551,7 @@ public class matchbot extends SubspaceBot {
     }
 
     public void parseCommand(String name, String command, String[] parameters, boolean isStaff, boolean isRestrictedStaff) {
-        m_botAction.sendSmartPrivateMessage("ThePAP", "[" + name + "]; [" + command + "]; [" + parameters + "]; [" + Boolean.toString(isStaff) + "]; [" + Boolean.toString(isRestrictedStaff) + "]");
+        m_botAction.sendSmartPrivateMessage("ThePAP", "[" + name + "]; [" + command + "]; [" + Boolean.toString(isStaff) + "]; [" + Boolean.toString(isRestrictedStaff) + "]; " + Boolean.toString(twdops.contains(name)));
         
         if (name.equalsIgnoreCase("ThePAP") && command.equals("!listops")) {
             for(int i = 0; i < twdops.size(); i++) {
