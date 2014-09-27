b1439c3a4a1943d8039f2571ac7114d95f6fc26b
G Dugwyler <dugwyler@gmail.com>
2014-09-27 04:42:09 +0000

    Making it its own module; forgot to check if TWD pubbot/hub module is actually used, heh.
    git-svn-id: http://www.twcore.org/svn/trunk@8891 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/pubhub/pubhublagwatch.java
@@ -0,0 +1,141 @@
+package twcore.bots.pubhub;
+
+import java.text.SimpleDateFormat;
+import java.util.TreeMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.Date;
+
+import twcore.bots.PubBotModule;
+import twcore.core.EventRequester;
+import twcore.core.events.Message;
+import twcore.core.events.InterProcessEvent;
+import twcore.core.util.ipc.IPCMessage;
+import twcore.core.util.Tools;
+
+public class pubhublagwatch extends PubBotModule {
+
+    /*
+     * String[] object of games
+     *0 - Arena Game is In
+     *1 - Freq 1 ID
+     *2 - Freq 2 ID
+     *3 - MatchBot hosting game
+     */
+    private String webdb = "website";
+    private String PUBBOTS = "pubBots";
+    private HashSet<String> lagWatched;
+    private Date date;
+    private SimpleDateFormat datetimeFormat = new SimpleDateFormat("[yyyy-MM-dd HH:mm] ");
+
+    public void initializeModule(){
+        lagWatched = new HashSet<String>();
+
+        try {
+            ResultSet r = m_botAction.SQLQuery(webdb, "SELECT fcName FROM tblLagWatch");
+            String player;
+            while (r.next()) {
+                player = r.getString("fcName");
+                lagWatched.add(player);
+                m_botAction.ipcTransmit(PUBBOTS, new IPCMessage("lagwatchon " + player));           
+            }
+            m_botAction.SQLClose(r);
+        } catch (Exception e) {
+            Tools.printLog("Problem loading LagWatch names from tblLagWatch.");
+        }
+    }
+
+    public void requestEvents(EventRequester r){
+        r.request(EventRequester.MESSAGE);
+    }
+
+    public void cancel() {
+        lagWatched.clear();
+    }
+
+    public void handleEvent(Message event) {
+        String name = event.getMessager() == null ? m_botAction.getPlayerName(event.getPlayerID()) : event.getMessager();
+        String message = event.getMessage();
+        int messageType = event.getMessageType();
+        if (!opList.isSmod(name))
+            return;
+
+        if(messageType == Message.PRIVATE_MESSAGE || messageType == Message.REMOTE_PRIVATE_MESSAGE || messageType == Message.CHAT_MESSAGE ){
+            if(message.equalsIgnoreCase("!lagwatchinfo "))
+                doLagWatchInfo(name, message.substring(14));
+            if(message.equalsIgnoreCase("!lagwatchon "))
+                doLagWatchOn(message.substring(12));
+            else if(message.equalsIgnoreCase("!lagwatchoff "))
+                doLagWatchOff(message.substring(13));
+            else if(message.equalsIgnoreCase("!lagwatchclear "))
+                doLagWatchClear(message.substring(15));
+            else if(message.equalsIgnoreCase("!lagwatches"))
+                doLagWatches();
+        }
+    }
+    
+    public void doLagWatchOn(String player) {
+        try {
+            lagWatched.add(player);
+            m_botAction.SQLQueryAndClose(webdb, "INSERT INTO tblLagWatch (fcName) VALUES ('" + Tools.addSlashesToString(player) + "')");
+            m_botAction.ipcTransmit(PUBBOTS, new IPCMessage("lagwatchon " + player));           
+            m_botAction.sendChatMessage("Recording lag info for '" + player + "' after entering any pubbot-enabled arena. !lagwatchoff to disable.");
+        } catch (Exception e) {
+            m_botAction.sendChatMessage("Could not add player '" + player + "' -- already watched? Check !lagwatches.");
+        }
+    }
+
+    public void doLagWatchOff(String player) {
+        if (!lagWatched.contains(player)) {
+            m_botAction.sendChatMessage("Not currently watching '" + player + "' -- please check spelling.");
+            return;
+        }
+        try {
+            lagWatched.remove(player);
+            m_botAction.SQLQueryAndClose(webdb, "DELETE FROM tblLagWatch WHERE fcName='" + Tools.addSlashesToString(player) + "'");
+            m_botAction.ipcTransmit(PUBBOTS, new IPCMessage("lagwatchoff " + player));           
+            m_botAction.sendChatMessage("No longer recording lag info for '" + player + "'. !lagwatchinfo to see all recorded data, or !lagwatchclear to remove data when no longer needed.");
+        } catch (Exception e) {
+            m_botAction.sendChatMessage("Problem when removing player '" + player + "'.");
+        }
+    }
+    
+    public void doLagWatchClear(String player) {
+        try {
+            m_botAction.SQLQueryAndClose(webdb, "DELETE FROM tblLagWatchData WHERE fcName='" + Tools.addSlashesToString(player) + "'");
+            m_botAction.sendChatMessage("All data for '" + player + "' removed.");
+        } catch (Exception e) {
+            m_botAction.sendChatMessage("Problem when removing data for player '" + player + "'.");
+        }
+    }
+
+    public void doLagWatches() {
+        try {
+            String watchedList = "Currently watching: ";
+            for( String watched : lagWatched)
+                watchedList += (watched + " ");
+            m_botAction.sendChatMessage(watchedList);
+        } catch (Exception e) {
+            m_botAction.sendChatMessage("Problem encountered listing watched players.");
+        }
+    }
+    
+    public void doLagWatchInfo(String staffer, String player) {
+        try {
+            ResultSet r = m_botAction.SQLQuery(webdb, "SELECT * FROM tblLagWatchData WHERE fcName='" + Tools.addSlashesToString(player) + "'");
+            while (r.next()) {
+                String result = "";
+                result += datetimeFormat.format(r.getTimestamp("fdCreated"));
+                result += r.getString("fcLag");
+                m_botAction.sendSmartPrivateMessage(staffer, result);
+            }
+            m_botAction.SQLClose(r);
+        } catch (Exception e) {
+            m_botAction.sendChatMessage("Problem getting lagwatch info.");
+        }
+
+    }
+
+}
