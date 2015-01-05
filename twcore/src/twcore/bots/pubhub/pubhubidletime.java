866f6494d21eb81d8f6a07f9800df60f0ee0d996
CRe <CRe>
2015-01-05 04:44:23 +0000

    So this loads.
    git-svn-id: http://www.twcore.org/svn/trunk@8943 9f8b3893-2a26-0410-9d00-a71e40fd2f77

---
+++ twcore/src/twcore/bots/pubhub/pubhubidletime.java
@@ -0,0 +1,26 @@
+package twcore.bots.pubhub;
+
+import twcore.bots.PubBotModule;
+import twcore.core.EventRequester;
+
+public class pubhubidletime extends PubBotModule {
+
+	@Override
+	public void initializeModule() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void requestEvents(EventRequester eventRequester) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
