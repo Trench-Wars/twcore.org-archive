44314d98cef344ba3cc6f1d196e381de6c82c61a
CRe <CRe>
2015-07-09 22:28:07 +0000

    Disabling money LVZ panel to prevent spam.
    git-svn-id: http://www.twcore.org/svn/trunk@8999 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- twcore/src/twcore/bots/pubsystem/module/moneysystem/LvzMoneyPanel.java
+++ twcore/src/twcore/bots/pubsystem/module/moneysystem/LvzMoneyPanel.java
@@ -4,14 +4,14 @@ import twcore.core.BotAction;
 
 public class LvzMoneyPanel {
 
-    private BotAction botAction;
+    //private BotAction botAction;
     
     // debug purpose only
     private static long startedAt = System.currentTimeMillis();
     public static long totalObjSent = 0;
     
     public LvzMoneyPanel(BotAction botAction){
-        this.botAction = botAction;
+        //this.botAction = botAction;
     }
     
     /* This algorithm try to avoid unnecessary changes when the object is already on/off
@@ -19,53 +19,11 @@ public class LvzMoneyPanel {
      */
 	public void update(int playerId, String beforeMoney, String afterMoney, boolean gainedMoney){
 
-    	if (beforeMoney.equals("0"))
-    		beforeMoney = "";
-    	
-    	// Padding with empty space
-        if (afterMoney.length() > beforeMoney.length()) {
-        	beforeMoney = String.format("%1$" + afterMoney.length() + "s", beforeMoney);  
-        } else if (afterMoney.length() < beforeMoney.length()) {
-        	afterMoney = String.format("%1$" + beforeMoney.length() + "s", afterMoney); 
-        }
-
-        String playerName = botAction.getPlayerName(playerId);
-        
-        int length = afterMoney.length();
-        
-        for(int i = 0; i < afterMoney.length(); i++)
-        {
- 
-        	if (beforeMoney.charAt(i) == afterMoney.charAt(i)) {
-        		continue;
-        	}
-        	else if (afterMoney.charAt(i) == ' ') {
-        		botAction.sendUnfilteredPrivateMessage(playerName, "*objoff "+502+(length-i-1)+beforeMoney.charAt(i));
-        	}
-        	else {
-        		if (beforeMoney.charAt(i) != ' ') {
-        			botAction.sendUnfilteredPrivateMessage(playerName, "*objoff "+502+(length-i-1)+beforeMoney.charAt(i));
-        			totalObjSent++;
-        		}
-        		botAction.sendUnfilteredPrivateMessage(playerName, "*objon "+502+(length-i-1)+afterMoney.charAt(i));
-        	}
-        	totalObjSent++;
-
-        }
-        
-        if(gainedMoney)
-            botAction.sendUnfilteredPrivateMessage(playerName, "*objon 50100");
-        else
-            botAction.sendUnfilteredPrivateMessage(playerName, "*objon 50101");
-        totalObjSent++;
+    	return;
     }
     
     public void reset(String playerName, int oldValue) {
-    	
-    	String value = String.valueOf(oldValue);
-    	for(int i = 0; i < value.length(); i++) {
-    		botAction.sendUnfilteredPrivateMessage(playerName, "*objoff "+502+(value.length()-i-1)+value.charAt(i));
-    	}
+        return;
     	
     }
     
@@ -76,11 +34,7 @@ public class LvzMoneyPanel {
     }
     
     public void reset(String playerName){
-		for (int i = 0; i < 7; i++) {
-			for (int j = 0; j < 10; j++) {
-				botAction.sendUnfilteredPrivateMessage(playerName, "*objoff " + 502 + i + j);
-			}
-		}
+		return;
     }
 
     
