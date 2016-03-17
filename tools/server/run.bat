e76e0ff411028e13932a10a4690d53c006789f90
milosh <miloshtw@gmail.com>
2016-03-17 13:49:15 +0000

    Separate Zone and tools from main repository.
    git-svn-id: http://www.twcore.org/svn/trunk@9372 9f8b3893-2a26-0410-9d00-a71e40fd2f77

--- tools/server/run.bat
+++
@@ -1,2 +0,0 @@
-start Subbill.exe
-start subgame2.exe
--- twcore-asss/src/twcore/bots/pubsystem/util/IPCReceiver.java
+++
@@ -1,9 +0,0 @@
-package twcore.bots.pubsystem.util;
-
-import twcore.core.events.InterProcessEvent;
-
-public interface IPCReceiver {
-
-	public void handleInterProcessEvent(InterProcessEvent event);
-	
-}
